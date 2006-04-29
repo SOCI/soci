7a3f4254628f032d42f7e48a7be598ba1102ef7e
pfedor <pfedor>
2006-04-29 21:39:18 +0000

    Split mysql backend source into many files. Added test17 and test18 (taken from test-postgresql.cpp)

---
+++ backends/mysql/standard-use-type.cpp
@@ -0,0 +1,166 @@
+//
+// Copyright (C) 2004-2006 Maciej Sobczak, Stephen Hutton
+// MySQL backend copyright (C) 2006 Pawel Aleksander Fedorynski
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "soci.h"
+#include "soci-mysql.h"
+
+#include <cerrno>
+
+#ifdef _MSC_VER
+#pragma warning(disable:4355)
+#endif
+
+using namespace SOCI;
+using namespace SOCI::details;
+
+
+void MySQLStandardUseTypeBackEnd::bindByPos(
+    int &position, void *data, eExchangeType type)
+{
+    data_ = data;
+    type_ = type;
+    position_ = position++;
+}
+
+void MySQLStandardUseTypeBackEnd::bindByName(
+    std::string const &name, void *data, eExchangeType type)
+{
+    data_ = data;
+    type_ = type;
+    name_ = name;
+}
+
+namespace { // anonymous
+
+char * quote(MYSQL * conn, const char *s, int l)
+{
+    char *retv = new char[2 * l + 3];
+    retv[0] = '\'';
+    int le = mysql_real_escape_string(conn, retv + 1, s, l);
+    retv[le + 1] = '\'';
+    retv[le + 2] = '\0';
+
+    return retv;
+}
+
+} // namespace anonymous
+
+void MySQLStandardUseTypeBackEnd::preUse(eIndicator const *ind)
+{
+    if (ind != NULL && *ind == eNull)
+    {
+        buf_ = new char[5];
+        strcpy(buf_, "NULL");
+    }
+    else
+    {
+        // allocate and fill the buffer with text-formatted client data
+        switch (type_)
+        {
+        case eXChar:
+            {
+                char buf[] = { *static_cast<char*>(data_), '\0' };
+                buf_ = quote(statement_.session_.conn_, buf, 1);
+            }
+            break;
+        case eXCString:
+            {
+                CStringDescriptor *strDescr
+                    = static_cast<CStringDescriptor *>(data_);
+                buf_ = quote(statement_.session_.conn_, strDescr->str_,
+			     std::strlen(strDescr->str_));
+            }
+            break;
+        case eXStdString:
+            {
+                std::string *s = static_cast<std::string *>(data_);
+                buf_ = quote(statement_.session_.conn_,
+			     s->c_str(), s->size());
+            }
+            break;
+        case eXShort:
+            {
+                std::size_t const bufSize
+                    = std::numeric_limits<short>::digits10 + 3;
+                buf_ = new char[bufSize];
+                std::snprintf(buf_, bufSize, "%d",
+                    static_cast<int>(*static_cast<short*>(data_)));
+            }
+            break;
+        case eXInteger:
+            {
+                std::size_t const bufSize
+                    = std::numeric_limits<int>::digits10 + 3;
+                buf_ = new char[bufSize];
+                std::snprintf(buf_, bufSize, "%d",
+                    *static_cast<int*>(data_));
+            }
+            break;
+        case eXUnsignedLong:
+            {
+                std::size_t const bufSize
+                    = std::numeric_limits<unsigned long>::digits10 + 2;
+                buf_ = new char[bufSize];
+                std::snprintf(buf_, bufSize, "%lu",
+                    *static_cast<unsigned long*>(data_));
+            }
+            break;
+        case eXDouble:
+            {
+                // no need to overengineer it (KISS)...
+
+                std::size_t const bufSize = 100;
+                buf_ = new char[bufSize];
+
+                std::snprintf(buf_, bufSize, "%.20g",
+                    *static_cast<double*>(data_));
+            }
+            break;
+        case eXStdTm:
+            {
+                std::size_t const bufSize = 22;
+                buf_ = new char[bufSize];
+
+                std::tm *t = static_cast<std::tm *>(data_);
+                std::snprintf(buf_, bufSize,
+                    "\'%d-%02d-%02d %02d:%02d:%02d\'",
+                    t->tm_year + 1900, t->tm_mon + 1, t->tm_mday,
+                    t->tm_hour, t->tm_min, t->tm_sec);
+            }
+            break;
+        default:
+            throw SOCIError("Use element used with non-supported type.");
+        }
+    }
+
+    if (position_ > 0)
+    {
+        // binding by position
+        statement_.useByPosBuffers_[position_] = &buf_;
+    }
+    else
+    {
+        // binding by name
+        statement_.useByNameBuffers_[name_] = &buf_;
+    }
+}
+
+void MySQLStandardUseTypeBackEnd::postUse(bool gotData, eIndicator *ind)
+{
+    cleanUp();
+}
+
+void MySQLStandardUseTypeBackEnd::cleanUp()
+{
+    if (buf_ != NULL)
+    {
+        delete [] buf_;
+        buf_ = NULL;
+    }
+}
+
