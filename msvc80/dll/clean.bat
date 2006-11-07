c69a6c3a04921843333da6bda1c0755c5cbe8e8c
mloskot <mloskot>
2006-11-07 19:53:23 +0000

    Added Visual C++ 2005 project files for building SOCI as DLL and Static libraries.

---
+++ msvc80/dll/clean.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+SET PROJECT=soci_dll
+SET TARGET=Clean
+SET TARGETCONFIG=Debug
+SET TARGETPLATFORM=Win32
+SET CLP=PerformanceSummary;NoSummary
+
+REM
+REM Clean intermediate Visual C++ project files
+REM
+
+IF EXIST *.vcproj.*.user DEL /Q *.vcproj.*.user
+IF EXIST *.ncb DEL /Q *.ncb
+
+ECHO ===============================================
+ECHO   Clean '%PROJECT%' project binaries
+ECHO ===============================================
+
+IF NOT EXIST %PROJECT%.sln GOTO no_sln_file
+
+msbuild %PROJECT%.sln /t:%TARGET% /p:Configuration=%TARGETCONFIG% /p:Platform=%TARGETPLATFORM% /clp:%CLP% /v:normal /nologo
+EXIT /B 0
+
+:no_sln_file
+ECHO ERROR: Can not find %PROJECT%.sln file!
+EXIT /B 1
---
+++ msvc80/lib/build.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+SET PROJECT=soci_lib
+SET TARGET=Build
+SET TARGETCONFIG=Debug
+SET TARGETPLATFORM=Win32
+SET CLP=PerformanceSummary;NoSummary
+
+
+ECHO ===============================================
+ECHO   Build '%PROJECT%' project
+ECHO ===============================================
+
+IF NOT EXIST %PROJECT%.sln GOTO no_sln_file
+
+msbuild %PROJECT%.sln /t:%TARGET% /p:Configuration=%TARGETCONFIG% /p:Platform=%TARGETPLATFORM% /clp:%CLP% /v:normal /nologo
+EXIT /B 0
+
+:no_sln_file
+ECHO ERROR: Can not find %PROJECT%.sln file!
+EXIT /B 1
