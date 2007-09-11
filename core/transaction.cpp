93bd3aa68e47b6089c239b0e3bca4facdb56c2c9
msobczak <msobczak>
2007-09-11 14:34:18 +0000

    Added transactions.

---
+++ core/transaction.cpp
@@ -0,0 +1,51 @@
+//
+// Copyright (C) 2004-2007 Maciej Sobczak
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include "transaction.h"
+#include "error.h"
+
+using namespace soci;
+
+transaction::transaction(session &sql, std::string const &startCommand)
+    : handled_(false), sql_(sql)
+{
+    sql_ << startCommand;
+}
+
+transaction::~transaction()
+{
+    if (handled_ == false)
+    {
+        try
+        {
+            rollback();
+        }
+        catch (...) {}
+    }
+}
+
+void transaction::commit()
+{
+    if (handled_)
+    {
+        throw soci_error("The transaction object cannot be handled twice.");
+    }
+
+    sql_ << "commit";
+    handled_ = true;
+}
+
+void transaction::rollback()
+{
+    if (handled_)
+    {
+        throw soci_error("The transaction object cannot be handled twice.");
+    }
+
+    sql_ << "rollback";
+    handled_ = true;
+}
---
+++ core/transaction.h
@@ -0,0 +1,37 @@
+//
+// Copyright (C) 2004-2007 Maciej Sobczak
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef TRANSACTION_H_INCLUDED
+#define TRANSACTION_H_INCLUDED
+
+#include "session.h"
+#include "soci-config.h"
+
+#include <string>
+
+namespace soci
+{
+
+class SOCI_DECL transaction
+{
+public:
+    transaction(session &sql,
+        std::string const & startCommand = "start transaction");
+
+    ~transaction();
+
+    void commit();
+    void rollback();
+
+private:
+    bool handled_;
+    session &sql_;
+};
+
+} // namespace soci
+
+#endif // TRANSACTION_H_INCLUDED
