e05ca7849d31c3cbcfe250fafe0cd49798d45320
Mateusz Loskot <mateusz@loskot.net>
2010-02-22 23:32:32 +0000

    Unscreamified FindSQLite3.cmake

--- src/cmake/modules/FindSQLite3.cmake
+++ src/cmake/modules/FindSQLite3.cmake
@@ -15,24 +15,24 @@
 ###############################################################################
 #MESSAGE(STATUS "Searching for SpatialIndex ${SpatialIndex_FIND_VERSION}+ library")
 #MESSAGE(STATUS " NOTE: Required version is not checked - to be implemented")
- 
-IF(SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY)
-    # Already in cache, be silent
-    SET(SQLITE3_FIND_QUIETLY TRUE)
-ELSE()
- 
-  IF(WIN32)
-    SET(OSGEO4W_IMPORT_LIBRARY sqlite3_i)
-    IF(DEFINED ENV{OSGEO4W_ROOT})
-        SET(OSGEO4W_ROOT_DIR $ENV{OSGEO4W_ROOT})
-        MESSAGE(STATUS "Trying OSGeo4W using environment variable OSGEO4W_ROOT=$ENV{OSGEO4W_ROOT}")
-    ELSE()
-        SET(OSGEO4W_ROOT_DIR c:/OSGeo4W)
-        MESSAGE(STATUS "Trying OSGeo4W using default location OSGEO4W_ROOT=${OSGEO4W_ROOT_DIR}")
-    ENDIF()
-  ENDIF()
- 
-  FIND_PATH(SQLITE3_INCLUDE_DIR
+
+if(SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY)
+  # Already in cache, be silent
+  set(SQLITE3_FIND_QUIETLY TRUE)
+else()
+
+  if(WIN32)
+    set(OSGEO4W_IMPORT_LIBRARY sqlite3_i)
+    if(DEFINED ENV{OSGEO4W_ROOT})
+      set(OSGEO4W_ROOT_DIR $ENV{OSGEO4W_ROOT})
+      message(STATUS "Trying OSGeo4W using environment variable OSGEO4W_ROOT=$ENV{OSGEO4W_ROOT}")
+    else()
+      set(OSGEO4W_ROOT_DIR c:/OSGeo4W)
+      message(STATUS "Trying OSGeo4W using default location OSGEO4W_ROOT=${OSGEO4W_ROOT_DIR}")
+    endif()
+  endif()
+
+  find_path(SQLITE3_INCLUDE_DIR
     NAMES sqlite3.h
     PATH_PREFIXES sqlite sqlite3
     PATHS
@@ -46,9 +46,9 @@ ELSE()
     $ENV{SystemDrive}/SQLite/*/include
     $ENV{SystemDrive}/SQLite3/*/include
     ${OSGEO4W_ROOT_DIR}/include)
- 
-  SET(SQLITE3_NAMES ${OSGEO4W_IMPORT_LIBRARY} sqlite3)
-  FIND_LIBRARY(SQLITE3_LIBRARY
+
+  set(SQLITE3_NAMES ${OSGEO4W_IMPORT_LIBRARY} sqlite3)
+  find_library(SQLITE3_LIBRARY
     NAMES sqlite3
     PATHS
     /usr/lib
@@ -59,17 +59,17 @@ ELSE()
     $ENV{SystemDrive}/SQLite/*/lib
     $ENV{SystemDrive}/SQLite3/*/lib
     ${OSGEO4W_ROOT_DIR}/lib)
- 
+
   set(SQLITE3_LIBRARIES ${SQLITE3_LIBRARY})
 
   #message(STATUS ${SQLITE3_LIBRARY})
   # Handle the QUIETLY and REQUIRED arguments and set SQLITE3_FOUND to TRUE
   # if all listed variables are TRUE
-  INCLUDE(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
- 
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(SQLITE3 DEFAULT_MSG SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
+
   # TODO: Do we want to mark these as advanced? --mloskot
   # http://www.cmake.org/cmake/help/cmake2.6docs.html#command:mark_as_advanced
   #MARK_AS_ADVANCED(SQLITE3_LIBRARY SQLITE3_INCLUDE_DIR)
 
-ENDIF()
+endif()
