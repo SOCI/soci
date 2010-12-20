02e3fa7d81aad21b156f1810aafae248ee440e2b
Mateusz Loskot <mateusz@loskot.net>
2010-12-20 02:03:20 +0000

    Copied simplified versions of PostgreSQL internal string_to_integer and string_to_unsigned_integer functions to SQLit3 backend.

--- src/backends/sqlite3/common.h
+++ src/backends/sqlite3/common.h
@@ -9,6 +9,8 @@
 #define SOCI_SQLITE3_COMMON_H_INCLUDED
 
 #include <cstddef>
+#include <cstdio>
+#include <cstring>
 #include <ctime>
 #include <vector>
 
@@ -30,6 +32,52 @@ void resize_vector(void *p, std::size_t sz)
 {
     std::vector<T> *v = static_cast<std::vector<T> *>(p);
     v->resize(sz);
+}
+
+// helper function for parsing integers
+template <typename T>
+T string_to_integer(char const * buf)
+{
+    long long t(0);
+    int n(0);
+    int const converted = std::sscanf(buf, "%lld%n", &t, &n);
+    if (converted == 1 && static_cast<std::size_t>(n) == std::strlen(buf))
+    {
+        // successfully converted to long long
+        // and no other characters were found in the buffer
+
+        const T max = (std::numeric_limits<T>::max)();
+        const T min = (std::numeric_limits<T>::min)();
+        if (t <= static_cast<long long>(max) &&
+            t >= static_cast<long long>(min))
+        {
+            return static_cast<T>(t);
+        }
+    }
+
+    throw soci_error("Cannot convert data.");
+}
+
+// helper function for parsing unsigned integers
+template <typename T>
+T string_to_unsigned_integer(char const * buf)
+{
+    unsigned long long t(0);
+    int n(0);
+    int const converted = std::sscanf(buf, "%llu%n", &t, &n);
+    if (converted == 1 && static_cast<std::size_t>(n) == std::strlen(buf))
+    {
+        // successfully converted to unsigned long long
+        // and no other characters were found in the buffer
+
+        T const max = (std::numeric_limits<T>::max)();
+        if (t <= static_cast<unsigned long long>(max))
+        {
+            return static_cast<T>(t);
+        }
+    }
+
+    throw soci_error("Cannot convert data.");
 }
 
 }}} // namespace soci::details::sqlite3
