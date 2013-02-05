d7ca3a8daf1d1b39eb2982405d68c0fb73d6c2d2
Denis Chapligin <chollya@satgate.net>
2013-02-05 16:47:27 +0300

    Fixed libdir detection for db2

--- src/cmake/modules/FindDB2.cmake
+++ src/cmake/modules/FindDB2.cmake
@@ -1,8 +1,16 @@
 #TODO: Add detection on win32
 
+if(CMAKE_SIZEOF_VOID_P EQUAL 4)
+  set(DB2_LIBDIR "lib")
+else()
+  set(DB2_LIBDIR "lib64")
+endif()
+
+
 find_path(DB2_INCLUDE_DIR sqlcli1.h
   $ENV{DB2_INCLUDE_DIR}
   $ENV{DB2_DIR}/include
+  /opt/ibm/db2/V10.1/include
   /opt/ibm/db2/V9.7/include
   /opt/ibm/db2/V9.5/include
   /opt/ibm/db2/V9.1/include)
@@ -10,12 +18,11 @@ find_path(DB2_INCLUDE_DIR sqlcli1.h
 find_library(DB2_LIBRARY NAMES db2
     PATHS
     $ENV{DB2_LIB_DIR}
-    $ENV{DB2_DIR}/lib32
-    $ENV{DB2_DIR}/lib64
-  /opt/ibm/db2/V9.7/lib32
-  /opt/ibm/db2/V9.5/lib64
-  /opt/ibm/db2/V9.1/lib32
-  /opt/ibm/db2/V9.1/lib64)
+    $ENV{DB2_DIR}/${DB2_LIBDIR}
+  /opt/ibm/db2/V10.1/${DB2_LIBDIR}
+  /opt/ibm/db2/V9.7/${DB2_LIBDIR}
+  /opt/ibm/db2/V9.5/${DB2_LIBDIR}
+  /opt/ibm/db2/V9.1/${DB2_LIBDIR})
 
 if(DB2_LIBRARY)
   get_filename_component(DB2_LIBRARY_DIR ${DB2_LIBRARY} PATH)
