8aca37fbcfbabfde3c65597aa12f5b140203c09b
Mateusz Loskot <mateusz@loskot.net>
2013-03-06 23:34:46 +0400

    apt-get install libstdc++5 for Oracle (#92)

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -10,7 +10,7 @@ sudo bash -c 'echo "deb https://oss.oracle.com/debian unstable main non-free" >>
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo apt-get update -qq
 sudo apt-get install -qq \
-	libboost-dev libboost-date-time-dev \
+    libstdc++5 libboost-dev libboost-date-time-dev \
 	libmyodbc unixodbc-dev odbc-postgresql \
 	firebird2.5-super firebird2.5-dev \
 	oracle-xe-client
