7c66b43b85f5f6fbda4d8c093b1b8792e2f5cbe6
Mateusz Loskot <mateusz@loskot.net>
2013-03-07 04:37:17 +0400

    Use different Oracle user for g++ and clang (#96)
    Enable g++ back - testing.

--- bin/ci/script.sh
+++ bin/ci/script.sh
@@ -7,13 +7,20 @@ source ./bin/ci/common.sh
 mkdir -p src/_build
 cd src/_build
 echo "$(tmstamp) *** script::cmake-config starting $(date) ***"
+if [ "${CXX}" == "g++" ]
+then
+    ORACLE_USER="soci_tester"
+else
+    ORACLE_USER="soci_tester1"
+fi
+
 cmake \
 	-DSOCI_STATIC=OFF \
 	-DSOCI_TESTS=ON \
 	-DSOCI_EMPTY_TEST_CONNSTR:STRING="dummy connection" \
 	-DSOCI_FIREBIRD_TEST_CONNSTR:STRING="service=LOCALHOST:/tmp/soci_test.fdb user=SYSDBA password=masterkey" \
 	-DSOCI_MYSQL_TEST_CONNSTR:STRING="db=soci_test" \
-	-DSOCI_ORACLE_TEST_CONNSTR:STRING="service=brzuchol.loskot.net user=soci_tester password=soci_secret" \
+	-DSOCI_ORACLE_TEST_CONNSTR:STRING="service=brzuchol.loskot.net user=${ORACLE_USER} password=soci_secret" \
 	-DSOCI_POSTGRESQL_TEST_CONNSTR:STRING="dbname=soci_test user=postgres" \
 	-DSOCI_SQLITE3_TEST_CONNSTR:STRING="soci_test.db" \
 	-DSOCI_ODBC_TEST_POSTGRESQL_CONNSTR="FILEDSN=${PWD}/../backends/odbc/test/test-postgresql.dsn;" \
