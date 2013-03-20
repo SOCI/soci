ade5a375581c3439598867a965f342d517e97f44
pacocamberos <pacocamberos@hotmail.com>
2013-03-20 13:02:22 -0400

    To enable BLOB support in MySQL.

--- src/backends/mysql/blob.cpp
+++ src/backends/mysql/blob.cpp
@@ -9,6 +9,7 @@
 #define SOCI_MYSQL_SOURCE
 #include "soci-mysql.h"
 #include <ciso646>
+#include <cstring>
 
 #ifdef _MSC_VER
 #pragma warning(push)
@@ -19,43 +20,111 @@ using namespace soci;
 using namespace soci::details;
 
 mysql_blob_backend::mysql_blob_backend(mysql_session_backend &session)
-    : session_(session)
+    : session_(session), buf_(0), len_(0)
 {
-    throw soci_error("BLOBs are not supported.");
 }
 
 mysql_blob_backend::~mysql_blob_backend()
 {
+	if (buf_)
+	{
+		delete [] buf_;
+		buf_ = 0;
+		len_ = 0;
+	}
 }
 
 std::size_t mysql_blob_backend::get_len()
 {
-    throw soci_error("BLOBs are not supported.");
+	return len_;
 }
 
 std::size_t mysql_blob_backend::read(
-    std::size_t /* offset */, char * /* buf */, std::size_t /* toRead */)
+	    std::size_t offset, char * buf, std::size_t toRead)
 {
-    throw soci_error("BLOBs are not supported.");
+	size_t r = toRead;
+
+	// make sure that we don't try to read
+	// past the end of the data
+	if (r > len_ - offset)
+	{
+		r = len_ - offset;
+	}
+
+	memcpy(buf, buf_ + offset, r);
+
+	return r;
 }
 
 std::size_t mysql_blob_backend::write(
-    std::size_t /* offset */, char const * /* buf */,
-    std::size_t /* toWrite */)
+	    std::size_t offset, char const * buf,
+	    std::size_t toWrite)
 {
-    throw soci_error("BLOBs are not supported.");
+	const char* oldBuf = buf_;
+	std::size_t oldLen = len_;
+	len_ = (std::max)(len_, offset + toWrite);
+
+	buf_ = new char[len_];
+
+	if (oldBuf)
+	{
+		// we need to copy both old and new buffers
+		// it is possible that the new does not
+		// completely cover the old
+		memcpy(buf_, oldBuf, oldLen);
+		delete [] oldBuf;
+	}
+	memcpy(buf_ + offset, buf, len_);
+
+	return len_;
 }
 
 std::size_t mysql_blob_backend::append(
-    char const * /* buf */, std::size_t /* toWrite */)
+	    char const * buf, std::size_t toWrite)
 {
-    throw soci_error("BLOBs are not supported.");
+    const char* oldBuf = buf_;
+
+    buf_ = new char[len_ + toWrite];
+
+    memcpy(buf_, oldBuf, len_);
+
+    memcpy(buf_ + len_, buf, toWrite);
+
+    delete [] oldBuf;
+
+    len_ += toWrite;
+
+    return len_;
 }
 
-void mysql_blob_backend::trim(std::size_t /* newLen */)
+void mysql_blob_backend::trim(std::size_t newLen)
 {
-    throw soci_error("BLOBs are not supported.");
+    const char* oldBuf = buf_;
+    len_ = newLen;
+
+    buf_ = new char[len_];
+
+    memcpy(buf_, oldBuf, len_);
+
+    delete [] oldBuf;
 }
+
+std::size_t mysql_blob_backend::set_data(char const *buf, std::size_t toWrite)
+{
+    if (buf_)
+    {
+        delete [] buf_;
+        buf_ = 0;
+        len_ = 0;
+    }
+    return write(0, buf, toWrite);
+}
+
+char *mysql_blob_backend::get_data()
+{
+	return buf_;
+}
+
 
 #ifdef _MSC_VER
 #pragma warning(pop)
