45e8f10a1791f3c45ae91020017c066d2e853355
Vadim Zeitlin <vz-soci@zeitlins.org>
2013-03-23 13:28:10 +0100

    Fix reading negative doubles from the database with Firebird backend.
    Mantissa of type SQL_LONG must be converted to "int", not "unsigned", when
    reading from the database.
    
    This corrects the changes of 4c3cb629 which did use int in other places where
    SQL_LONG is handled but not here to compensate for a different bug with
    handling of unsigned long which is fixed differently by the previous commit.
    
    Also add unit a (Firebird-specific for now but it should be made common) test
    for reading negative doubles.

--- src/backends/firebird/common.h
+++ src/backends/firebird/common.h
@@ -200,7 +200,7 @@ T1 from_isc(XSQLVAR * var)
     case SQL_SHORT:
         return static_cast<T1>(*reinterpret_cast<short*>(var->sqldata)/tens);
     case SQL_LONG:
-        return static_cast<T1>(*reinterpret_cast<unsigned*>(var->sqldata)/tens);
+        return static_cast<T1>(*reinterpret_cast<int*>(var->sqldata)/tens);
     case SQL_INT64:
         return static_cast<T1>(*reinterpret_cast<long long*>(var->sqldata)/tens);
     case SQL_FLOAT:
