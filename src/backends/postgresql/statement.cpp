SOCI/soci

commit 684be04744c46a89ad645a8ce4267a0a92690866

Fixes a crash and allows creating complex function
First fix around line 95 allows passing any query containing an assignment operator (:=) this way we can create complex functions within soci.
Second fix around line 172 avoids causing a second exception on the destructor when you have a 'statementName_' but actually PQPrepare() failed and threw a first exception.

src/backends/postgresql/statement.cpp
@@ -92,6 +92,13 @@ void postgresql_statement_backend::prepare(std::string const & query,
                     query_ += "::";
                     ++it;
                 }
+                // Check whether this is an assignment(e.g. x:=y)
+                // and treat it as a special case, not as a named binding
+                if ((next_it != end) && (*next_it == '='))
+                {
+                    query_ += ":=";
+                    ++it;
+                }
                 else
                 {
                     state = in_name;
@@ -160,14 +167,19 @@ void postgresql_statement_backend::prepare(std::string const & query,
 
     if (stType == st_repeatable_query)
     {
-        statementName_ = session_.get_next_statement_name();
+        // Holding the name temporarily in this var because
+        // if it fails to prepare it we can't DEALLOCATE it. 
+        std::string statementName = session_.get_next_statement_name();
 
-        PGresult* result = PQprepare(session_.conn_, statementName_.c_str(),
+        PGresult* result = PQprepare(session_.conn_, statementName.c_str(),
             query_.c_str(), static_cast<int>(names_.size()), NULL);
         if (result == NULL)
         {
             throw soci_error("Cannot prepare statement.");
         }
+        // Now it's safe to save this info.
+        statementName_ = statementName;
+        
         ExecStatusType status = PQresultStatus(result);
         if (status != PGRES_COMMAND_OK)
         {

commit c8ad2a884473a707c7bd9e5580a7aea3ea8e4c39

PostgreSQL Regression from 684be04744c

src/backends/postgresql/statement.cpp
@@ -94,7 +94,7 @@ void postgresql_statement_backend::prepare(std::string const & query,
                 }
                 // Check whether this is an assignment(e.g. x:=y)
                 // and treat it as a special case, not as a named binding
-                if ((next_it != end) && (*next_it == '='))
+                else if ((next_it != end) && (*next_it == '='))
                 {
                     query_ += ":=";
                     ++it;