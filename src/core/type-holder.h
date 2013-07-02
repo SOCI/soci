7ae286dd148aa0e3b0de497de808ab3c7358ac5d
abc100m <abc100m@126.com>
2013-07-03 00:25:12 +0800

    edit convertible class. change strategy of implicit cast

--- src/core/type-holder.h
+++ src/core/type-holder.h
@@ -17,9 +17,16 @@ namespace soci
 namespace details
 {
 
-//convertible is for pass test at [commontests.h, test12()]. Otherwise the test is an undefined runtime error
-//default:: any type convert is allowed 
-template <typename from, typename to> class convertible{public:enum {IS = 1};};
+
+//this class is for pass test [commontests.h, test12()], 
+//otherwise the test will failed with undefined runtime error.
+//default:: any type convert is not allowed 
+template <typename from, typename to> 
+class convertible{public:enum {IS = 0};};
+
+template<typename T>
+class convertible<T, T>{public:enum {IS = 1};};
+
 
 // Base class holder + derived class type_holder for storing type data
 // instances in a container of holder objects
@@ -44,27 +51,27 @@ public:
 
         if  (ti == typeid(int))
         {
-            return (const T&)cast<int, T>(pos);          
+            return cast<int, T>(pos);          
         }
         else if (ti == typeid(double))
         {                    
-            return (const T&)cast<double, T>(pos);       
+            return cast<double, T>(pos);       
         }
         else if (ti == typeid(std::string))
         {
-            return (const T&)cast<std::string, T>(pos);
+            return cast<std::string, T>(pos);
         }
         else if (ti == typeid(long long))
         {
-            return (const T&)cast<long long, T>(pos);
+            return cast<long long, T>(pos);
         }
         else if (ti == typeid(unsigned long long))
         {
-            return (const T&)cast<unsigned long long, T>(pos);
+            return cast<unsigned long long, T>(pos);
         }
         else if (ti == typeid(std::tm))
         {
-            return (const T&)cast<std::tm, T>(pos);
+            return cast<std::tm, T>(pos);
         }
         else
         {
@@ -75,15 +82,25 @@ public:
     virtual const std::type_info & type() const = 0;
 
 private:
-    template<typename T, typename R>
-    T& cast(size_t pos)
+    template<typename Holder_T, typename T>
+    const T& cast(size_t pos)
     {
-        //T can convert to R. e.g.: T t; R r; t = r;
-        if (convertible<T, R>::IS)
+#ifdef _MSC_VER
+#pragma warning( push ) 
+#pragma warning(disable:4127)
+#endif // _MSC_VER
+
+        //Holder_T can convert to T.  sample code: T t; Holder_T s; t=s;
+        if (convertible<Holder_T, T>::IS)   //diable warning 4127
         {
-            vector_type_holder<T>* p = static_cast<vector_type_holder<T> *>(this);
-            return p->template value<T>(pos);
+            vector_type_holder<Holder_T>* p = 
+                static_cast<vector_type_holder<Holder_T> *>(this);
+            return (const T&)p->template value<Holder_T>(pos);
         }
+
+#ifdef _MSC_VER
+#pragma warning( pop ) 
+#endif // _MSC_VER
 
         throw std::bad_cast();
     }
@@ -108,57 +125,31 @@ private:
     std::vector<T>* vec_;
 };
 
+//partial convertible class
 #define CONVERTIBLE(T, R, yes_no) \
     template<> class convertible<T, R>{public:enum {IS = yes_no};}
 
-//std::string
-CONVERTIBLE(int, std::string, 0);
-CONVERTIBLE(double, std::string, 0);
-CONVERTIBLE(long long, std::string, 0);
-CONVERTIBLE(unsigned long long, std::string, 0);
-CONVERTIBLE(std::tm, std::string, 0);
-CONVERTIBLE(std::string, int, 0);
-CONVERTIBLE(std::string, double, 0);
-CONVERTIBLE(std::string, long long, 0);
-CONVERTIBLE(std::string, unsigned long long, 0);
-CONVERTIBLE(std::string, std::tm, 0);
-//std::tm
-CONVERTIBLE(int, std::tm, 0);
-CONVERTIBLE(double, std::tm, 0);
-CONVERTIBLE(long long, std::tm, 0);
-CONVERTIBLE(unsigned long long, std::tm, 0);
-CONVERTIBLE(std::tm, int, 0);
-CONVERTIBLE(std::tm, double, 0);
-CONVERTIBLE(std::tm, long long, 0);
-CONVERTIBLE(std::tm, unsigned long long, 0);
+#define INTEGRAL_CONVERTIBLE(T) \
+    CONVERTIBLE(T, bool, 1);\
+    CONVERTIBLE(T, char, 1);\
+    CONVERTIBLE(T, unsigned char, 1);\
+    CONVERTIBLE(T, short, 1);\
+    CONVERTIBLE(T, unsigned short, 1);\
+    CONVERTIBLE(T, int, 1);\
+    CONVERTIBLE(T, unsigned int, 1);\
+    CONVERTIBLE(T, long, 1);\
+    CONVERTIBLE(T, unsigned long, 1);\
+    CONVERTIBLE(T, long long, 1);\
+    CONVERTIBLE(T, unsigned long long, 1);\
+    CONVERTIBLE(T, float, 1);\
+    CONVERTIBLE(T, double, 1);\
+    CONVERTIBLE(T, long double, 1)
 
-
-
-/*
-//string
-template<> class convertible<int,                string>{public:enum {IS = 0};};
-template<> class convertible<double,             string>{public:enum {IS = 0};};
-template<> class convertible<long long,          string>{public:enum {IS = 0};};
-template<> class convertible<unsigned long long, string>{public:enum {IS = 0};};
-template<> class convertible<std::tm,            string>{public:enum {IS = 0};};
-template<> class convertible<string, int               >{public:enum {IS = 0};};
-template<> class convertible<string, double            >{public:enum {IS = 0};};
-template<> class convertible<string, long long         >{public:enum {IS = 0};};
-template<> class convertible<string, unsigned long long>{public:enum {IS = 0};};
-template<> class convertible<string, std::tm           >{public:enum {IS = 0};};
-
-//std::tm
-template<> class convertible<int,                std::tm>{public:enum {IS = 0};};
-template<> class convertible<double,             std::tm>{public:enum {IS = 0};};
-template<> class convertible<long long,          std::tm>{public:enum {IS = 0};};
-template<> class convertible<unsigned long long, std::tm>{public:enum {IS = 0};};
-template<> class convertible<string,             std::tm>{public:enum {IS = 0};};
-template<> class convertible<std::tm, int               >{public:enum {IS = 0};};
-template<> class convertible<std::tm, double            >{public:enum {IS = 0};};
-template<> class convertible<std::tm, long long         >{public:enum {IS = 0};};
-template<> class convertible<std::tm, unsigned long long>{public:enum {IS = 0};};
-template<> class convertible<std::tm, string            >{public:enum {IS = 0};};
-*/
+//int, double, long long, unsigned long long
+INTEGRAL_CONVERTIBLE(int);
+INTEGRAL_CONVERTIBLE(double);
+INTEGRAL_CONVERTIBLE(long long);
+INTEGRAL_CONVERTIBLE(unsigned long long);
 
 
 } // namespace details
