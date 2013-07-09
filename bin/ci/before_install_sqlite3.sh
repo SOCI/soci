88cfa67bcdbb75c0f2dbb7690f53d68101248aef
Mateusz Loskot <mateusz@loskot.net>
2013-07-10 00:49:21 +0100

    Remove backend-specific commands from before_script.sh
    Remove no-op scripts, allow non-existent before_script scripts.

--- bin/ci/before_install_sqlite3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Run before_install for SOCI backend SQLite3 at travis-ci.org
-#
-# Copyright (c) 2013 Mateusz Loskot <mateusz@loskot.net>
-#
-source ./bin/ci/common.sh
-sudo apt-get install -qq libsqlite3-dev
-
