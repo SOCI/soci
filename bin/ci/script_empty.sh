ce8fd2537387593211c14893ff975739a7f4913a
Mateusz Loskot <mateusz@loskot.net>
2013-07-10 01:24:00 +0100

    Testing source location for CMake

--- bin/ci/script_empty.sh
+++ bin/ci/script_empty.sh
@@ -6,19 +6,5 @@
 source ${TRAVIS_BUILD_DIR}/bin/ci/common.sh
 echo $TRAVIS_BUILD_DIR
 echo $PWD
-cmake \
-    -DSOCI_STATIC=OFF \
-    -DSOCI_TESTS=ON \
-    -DSOCI_EMPTY=ON \ 
-    -DSOCI_EMPTY_TEST_CONNSTR:STRING="dummy connection" \
-    -DSOCI_DB2=OFF \ 
-    -DSOCI_FIREBIRD=OFF \ 
-    -DSOCI_MYSQL=OFF \ 
-    -DSOCI_ODBC=OFF \ 
-    -DSOCI_ORACLE=OFF \ 
-    -DSOCI_POSTGRESQL=OFF \ 
-    -DSOCI_SQLITE3=OFF ..
-
-[[ ${NUMTHREADS} -gt 0 ]] && make -j ${NUMTHREADS} || make
-ctest -V --output-on-failure .
+cmake ..
 
