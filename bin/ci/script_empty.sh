ad76b28f0b2117ed32ed01128b98e5568f8f5199
Mateusz Loskot <mateusz@loskot.net>
2013-07-19 01:17:26 +0100

    Still testing source locations

--- bin/ci/script_empty.sh
+++ bin/ci/script_empty.sh
@@ -5,8 +5,18 @@
 #
 source $TRAVIS_BUILD_DIR/bin/ci/common.sh
 
-cmake -DSOCI_STATIC=OFF \
-    $TRAVIS_BUILD_DIR/src
+cmake \
+    -DSOCI_STATIC=OFF \
+    -DSOCI_TESTS=ON \
+    -DSOCI_DB2=OFF \ 
+    -DSOCI_EMPTY=ON \ 
+    -DSOCI_FIREBIRD=OFF \ 
+    -DSOCI_MYSQL=OFF \ 
+    -DSOCI_ODBC=OFF \ 
+    -DSOCI_ORACLE=OFF \ 
+    -DSOCI_POSTGRESQL=OFF \
+    -DSOCI_SQLITE3=OFF \
+    ${TRAVIS_BUILD_DIR}/src
 
 make -j $TCI_NUMTHREADS
 ctest -V --output-on-failure .
