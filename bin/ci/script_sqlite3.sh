dbdb0198911b470c5968a88b9eccc65b42437daf
Mateusz Loskot <mateusz@loskot.net>
2013-07-20 00:36:14 +0100

    Adding CMake options one at time
    Debugging Travis CI behaviour

--- bin/ci/script_sqlite3.sh
+++ bin/ci/script_sqlite3.sh
@@ -3,22 +3,14 @@
 #
 # Copyright (c) 2013 Mateusz Loskot <mateusz@loskot.net>
 #
-source $TRAVIS_BUILD_DIR/bin/ci/common.sh
+source ${TRAVIS_BUILD_DIR}/bin/ci/common.sh
 
 cmake \
-    -DSOCI_STATIC=OFF \
     -DSOCI_TESTS=ON \
-    -DSOCI_DB2=OFF \ 
-    -DSOCI_EMPTY=OFF \ 
-    -DSOCI_FIREBIRD=OFF \ 
-    -DSOCI_MYSQL=OFF \ 
-    -DSOCI_ODBC=OFF \ 
-    -DSOCI_ORACLE=OFF \ 
-    -DSOCI_POSTGRESQL=OFF \
-    -DSOCI_SQLITE3=ON \
-    -DSOCI_SQLITE3_TEST_CONNSTR=\"soci_test.db\" \
+    -DSOCI_STATIC=OFF \
+    -DSOCI_DB2=OFF \
     ..
 
-make -j $TCI_NUMTHREADS
-ctest -V --output-on-failure .
+#make -j $TCI_NUMTHREADS
+#ctest -V --output-on-failure .
 
