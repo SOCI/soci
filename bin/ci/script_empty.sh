ca4b583a33ca77300bf3e9d52799d8267f75f8cc
Mateusz Loskot <mateusz@loskot.net>
2013-07-20 01:39:37 +0100

    Update script comments

--- bin/ci/script_empty.sh
+++ bin/ci/script_empty.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-# Run test script for SOCI backend empty at travis-ci.org
+# Builds and tests SOCI backend empty at travis-ci.org
 #
 # Copyright (c) 2013 Mateusz Loskot <mateusz@loskot.net>
 #
