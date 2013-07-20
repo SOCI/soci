1818bb01c67db61798e3836130e739869742aacd
Mateusz Loskot <mateusz@loskot.net>
2013-07-20 02:38:13 +0100

    Clean up common apt-get install line

--- bin/ci/before_install.sh
+++ bin/ci/before_install.sh
@@ -7,7 +7,7 @@ source ${TRAVIS_BUILD_DIR}/bin/ci/common.sh
 
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 16126D3A3E5C1192
 sudo apt-get update -qq
-sudo apt-get install -qq tar bzip2 libstdc++5 libboost-dev libboost-date-time-dev
+sudo apt-get install -qq libboost-dev libboost-date-time-dev
 
 before_install="${TRAVIS_BUILD_DIR}/bin/ci/before_install_${SOCI_TRAVIS_BACKEND}.sh"
 [ -x ${before_install} ] && ${before_install} || echo "nothing to run"
