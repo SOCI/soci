65a5ee3892570b1dc639b2dab10b26fc80a89582
Adesmier <arnaud@desmier.fr>
2013-08-05 11:47:57 +0200

    Append each member of tuple/fusion instead of adding tuple/fusion
     * Warning: break compatibility with boost 1.35 released in 2008!
     * when calling use() or into() with tuple or fusion objects, it stores
       reference to each member of object instead of storing object reference
       thanks to boost::fusion::foreach.
     * Job is done by new use_type_vector and into_type_vector classes

--- src/core/ref-counted-prepare-info.cpp
+++ src/core/ref-counted-prepare-info.cpp
@@ -12,18 +12,6 @@
 using namespace soci;
 using namespace soci::details;
 
-void ref_counted_prepare_info::exchange(into_type_ptr const & i)
-{
-    intos_.push_back(i.get());
-    i.release();
-}
-
-void ref_counted_prepare_info::exchange(use_type_ptr const & u)
-{
-    uses_.push_back(u.get());
-    u.release();
-}
-
 void ref_counted_prepare_info::final_action()
 {
     // deallocate all bind and define objects
