de7e6f080f850f4ff9ac338c3b9fdab65ee09a70
Mateusz Loskot <mateusz@loskot.net>
2014-03-31 23:14:11 +0200

    Bump library version number to 3.2.3

--- src/core/version.h
+++ src/core/version.h
@@ -20,13 +20,13 @@
 //  SOCI_VERSION / 100 % 1000 is the minor version
 //  SOCI_VERSION / 100000 is the major version
 
-#define SOCI_VERSION 300201
+#define SOCI_VERSION 300203
 
 //
 //  SOCI_LIB_VERSION must be defined to be the same as SOCI_VERSION
 //  but as a *string* in the form "x_y[_z]" where x is the major version
 //  number, y is the minor version number, and z is the patch level if not 0.
 
-#define SOCI_LIB_VERSION "3_2_2"
+#define SOCI_LIB_VERSION "3_2_3"
 
 #endif // SOCI_VERSION_HPP
