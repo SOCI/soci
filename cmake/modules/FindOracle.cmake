120b88c94c99350146e111a9f5557f2637753eda
Vadim Zeitlin <vz-soci@zeitlins.org>
2015-03-16 20:18:57 +0100

    Accept oraocci12 as a possible Oracle library name too.
    In addition to the already accepted oraocci10 and oraocci11 ones.
    
    Closes #271.

--- cmake/modules/FindOracle.cmake
+++ cmake/modules/FindOracle.cmake
@@ -37,7 +37,7 @@ if(DEFINED ENV{ORACLE_HOME})
     ${ORACLE_HOME}/OCI/include) # Oracle XE on Windows
 
   set(ORACLE_OCI_NAMES clntsh libclntsh oci) # Dirty trick might help on OSX, see issues/89
-  set(ORACLE_OCCI_NAMES libocci occi oraocci10 oraocci11)
+  set(ORACLE_OCCI_NAMES libocci occi oraocci10 oraocci11 oraocci12)
   set(ORACLE_NNZ_NAMES nnz10 libnnz10 nnz11 libnnz11 nnz12 libnnz12 ociw32)
 
   set(ORACLE_LIB_DIR
