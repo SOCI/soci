5115ee54c6ae44ef86a5aa695fc121f44613e211
tvb377 <titus@v9g.de>
2015-05-07 09:34:03 +0200

    VS 2015 does not like definition of snprintf
    Do not #define snprintf when using VC14.
    CRT headers for Windows Kit 10 error out when snprintf is defined as a
    macro.

--- include/soci/soci-platform.h
+++ include/soci/soci-platform.h
@@ -44,7 +44,9 @@
 #endif
 
 // Define if you have the snprintf variants.
+#if _MSC_VER < 1900
 #define snprintf _snprintf
+#endif
 
 // Define if you have the strtoll and strtoull variants.
 #if _MSC_VER < 1300
