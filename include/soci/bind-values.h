cf4a61bb64cd431e293f36da1653574db2d3e662
Sergei Nikulov <sergey.nikulov@gmail.com>
2015-10-09 14:19:52 +0300

    type_ptr update

--- include/soci/bind-values.h
+++ include/soci/bind-values.h
@@ -30,7 +30,7 @@ public:
             delete *iter;
     }
 
-    void exchange(use_type_ptr const& u) { push_back(u.get()); u.release(); }
+    void exchange(use_type_ptr const& u) { push_back(u.release()); }
 
     template <typename T, typename Indicator>
     void exchange(use_container<T, Indicator> const &uc)
@@ -120,7 +120,7 @@ public:
             delete *iter;
     }
 
-    void exchange(into_type_ptr const& i) { push_back(i.get()); i.release(); }
+    void exchange(into_type_ptr const& i) { push_back(i.release()); }
 
     template <typename T, typename Indicator>
     void exchange(into_container<T, Indicator> const &ic)
