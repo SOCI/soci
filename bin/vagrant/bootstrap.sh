435547064d816f0d380969f097ea46a1b9c35e4e
Mateusz Łoskot <mateusz@loskot.net>
2015-11-04 00:59:45 +0100

    Vagrant: define SOCI_* env variables via /etc/profile.d

--- bin/vagrant/bootstrap.sh
+++ bin/vagrant/bootstrap.sh
@@ -2,8 +2,8 @@
 # Part of Vagrant virtual development environments for SOCI
 
 # Pre-installation
-echo "Bootstrap: updating environment in /home/vagrant/.bashrc"
-echo "source /vagrant/bin/vagrant/common.env" >> /home/vagrant/.bashrc
+echo "Bootstrap: setting common environment in /etc/profile.d/vagrant-soci.sh"
+sudo sh -c "cat /vagrant/bin/vagrant/common.env > /etc/profile.d/vagrant-soci.sh"
 export DEBIAN_FRONTEND="noninteractive"
 # Installation
 # TODO: Switch to apt-fast when it is avaiable for Trusty
