98ae7b206dd294f6fd3d4b5fe9daa0199e8dcf29
unknown <MateuszL@DEV604.cadcorp.net>
2015-11-05 23:43:24 +0100

    Vagrant: try to avoid re-creation of soci DB if it exists
    [ci skip]

--- bin/vagrant/db2.sh
+++ bin/vagrant/db2.sh
@@ -8,5 +8,13 @@ export DEBIAN_FRONTEND="noninteractive"
 # Installation
 /vagrant/bin/ci/before_install_db2.sh
 # Post-installation
-sudo -u db2inst1 -i db2 "CREATE DATABASE ${SOCI_USER}"
-sudo -u db2inst1 -i db2 "ACTIVATE DATABASE ${SOCI_USER}"
+## Let's be gentle to DB2 and try to not to recreate databases
+echo "db2: checking if ${SOCI_USER} database exists"
+sudo -u db2inst1 -i db2 "LIST DATABASE DIRECTORY" | grep ${SOCI_USER}
+if [ $? -eq 0 ]; then
+  echo "db2: creating database ${SOCI_USER}"
+  sudo -u db2inst1 -i db2 "CREATE DATABASE ${SOCI_USER}"
+  sudo -u db2inst1 -i db2 "ACTIVATE DATABASE ${SOCI_USER}"
+else
+  echo "db2: database ${SOCI_USER} (may) exists, skipping"
+fi
