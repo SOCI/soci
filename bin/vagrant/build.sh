4a576a0c2d54694b9e1871e445e8427e947729d8
Mateusz Łoskot <mateusz@loskot.net>
2015-11-07 00:25:17 +0100

    Vagrant: switch back to building from /vagrant share
    Generate build into /home/vagrant which lives in Linux FS
    where symlinks are fully supported.
    
    [ci skip]

--- bin/vagrant/build.sh
+++ bin/vagrant/build.sh
@@ -4,23 +4,19 @@
 # Builds and tests SOCI from git master branch
 source /vagrant/bin/vagrant/common.env
 
-# Clone SOCI locally on Linux filesystem, instead of VM shared folder.
+# Build SOCI in /home/vagrant on Linux filesystem,
+# outside /vagrant which is VM shared directory.
 # Otherwise, CMake will fail:
 # CMake Error: cmake_symlink_library: System Error: Protocol error
 # Explanation from https://github.com/mitchellh/vagrant/issues/713
 # The VirtualBox shared folder filesystem doesn't allow symlinks, unfortunately.
 # Your only option is to deploy outside of the shared folders.
-if [[ ! -d "${SOCI_HOME}" && ! -d "${SOCI_HOME}/.git" ]] ; then
-  echo "Build: cloning SOCI master"
-  git clone https://github.com/SOCI/soci.git ${SOCI_HOME}
+if [[ ! -d "${SOCI_BUILD}" ]] ; then
+  mkdir -p ${SOCI_BUILD}
 fi
 
-if [[ ! -d "${SOCI_HOME}/_build" ]] ; then
-  mkdir ${SOCI_HOME}/_build
-fi
-
-echo "Build: building SOCI"
-cd ${SOCI_HOME}/_build && \
+echo "Build: building SOCI from sources in ${SOCI_HOME} to build in ${SOCI_BUILD}"
+cd ${SOCI_BUILD} && \
 cmake \
     -DSOCI_TESTS=ON \
     -DSOCI_STATIC=OFF \
@@ -35,8 +31,9 @@ cmake \
     -DSOCI_FIREBIRD_TEST_CONNSTR:STRING="service=LOCALHOST:/tmp/soci.fdb user=${SOCI_USER} password==${SOCI_PASS}" \
     -DSOCI_MYSQL_TEST_CONNSTR:STRING="host=localhost db=${SOCI_USER} user=${SOCI_USER} password==${SOCI_PASS}" \
     -DSOCI_POSTGRESQL_TEST_CONNSTR:STRING="host=localhost port=5432 dbname=${SOCI_USER} user=${SOCI_USER} password==${SOCI_PASS}" \
-    .. && \
+    ${SOCI_HOME} && \
 make
+
 # Do not run tests during provisioning, thay may fail terribly, so just build
 # and let to run them manually after developer vagrant ssh'ed to the VM.
 #ctest -V --output-on-failure .
