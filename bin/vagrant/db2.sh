c2a387b2f0d52fcd724ab8db53db74284edb2511
Mateusz Łoskot <mateusz@loskot.net>
2015-11-07 22:25:03 +0100

    Vagrant: correct DB2 database lookup for existing soci test db.
    [ci skip]

--- bin/vagrant/db2.sh
+++ bin/vagrant/db2.sh
@@ -8,10 +8,15 @@ export DEBIAN_FRONTEND="noninteractive"
 # Installation
 /vagrant/bin/ci/before_install_db2.sh
 # Post-installation
-## Let's be gentle to DB2 and try to not to recreate databases
+## Let's be gentle to DB2 and try to not to recreate existing databases
 echo "db2: checking if ${SOCI_USER} database exists"
-sudo -u db2inst1 -i db2 "LIST DATABASE DIRECTORY" | grep ${SOCI_USER}
-if [ $? -eq 0 ]; then
+# First time guest machine is created, there is no database, hence test for:
+# SQL1031N  The database directory cannot be found on the indicated file system.
+sudo -u db2inst1 -i db2 "LIST DATABASE DIRECTORY" | grep SQL1031N
+NODB=$?
+sudo -u db2inst1 -i db2 "LIST DATABASE DIRECTORY" | grep -i ${SOCI_USER}
+HASSOCI=$?
+if [[ $NODB -eq 0 || $HASSOCI -ne 0 ]]; then
   echo "db2: creating database ${SOCI_USER}"
   sudo -u db2inst1 -i db2 "CREATE DATABASE ${SOCI_USER}"
   sudo -u db2inst1 -i db2 "ACTIVATE DATABASE ${SOCI_USER}"
