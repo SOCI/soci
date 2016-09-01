be4977d7a630680f7c5fa4b9629a0ba6c977b3d6
Serhiy Ponomarets <ponomarets@apriorit.com>
2016-09-01 16:43:33 +0300

    Fix unittests

--- src/core/once-temp-type.cpp
+++ src/core/once-temp-type.cpp
@@ -37,14 +37,14 @@ once_temp_type & once_temp_type::operator=(once_temp_type const & o)
 
 once_temp_type::~once_temp_type() SOCI_NOEXCEPT_FALSE
 {
-    try
+    //try
     {
         rcst_->dec_ref();
     }
-    catch(...)
+    /*catch(...)
     {
         //Throwing an exception out of a destructor is forbidden
-    }
+    }*/
 }
 
 once_temp_type & once_temp_type::operator,(into_type_ptr const & i)
