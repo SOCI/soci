d676079ae49a5caad75a6e11b382b4363400508b
Mateusz Łoskot <mateusz@loskot.net>
2018-09-30 00:30:31 +0200

    Move forward with installing targets.
    Add targets for empty backend.

---
+++ cmake/soci-config.cmake
@@ -0,0 +1,11 @@
+################################################################################
+# This soci-config.cmake is a part of SOCI library.
+#
+# Copyright (C) 2018 Mateusz Loskot <mateusz@loskot.net>
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+#
+################################################################################
+include("${CMAKE_CURRENT_LIST_DIR}/soci-targets.cmake")
