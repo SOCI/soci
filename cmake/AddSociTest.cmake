55640f769f067eac7a50f1602ec92af03487f968
Leon De Andrade <leondeandrade@hotmail.com>
2020-07-05 19:42:34 +0200

    Create alias targets

--- cmake/AddSociTest.cmake
+++ cmake/AddSociTest.cmake
@@ -8,7 +8,7 @@ function(add_soci_test)
   )
 
   add_executable(soci_${ARG_NAME}_test)
-  target_link_libraries(soci_${ARG_NAME}_test PRIVATE soci_core soci_${ARG_NAME})
+  target_link_libraries(soci_${ARG_NAME}_test PRIVATE SOCI::soci_core SOCI::soci_${ARG_NAME})
   target_include_directories(soci_${ARG_NAME}_test 
     PRIVATE 
       ${SOCI_SOURCE_DIR}/include/private 
