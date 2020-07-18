527adec9c4c976a3b7814ae9dc119367dce5f87c
Leon De Andrade <leondeandrade@hotmail.com>
2020-07-18 19:59:00 +0200

    travis test

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -8,10 +8,11 @@ source ${TRAVIS_BUILD_DIR}/scripts/travis/common.sh
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
 sudo apt-add-repository -y 'deb https://apt.kitware.com/ubuntu/ xenial main'
 sudo apt-get update -qq -y
-sudo apt-get remove -y cmake # Remove travis cmake version
 sudo apt-get install -y libboost-dev libboost-date-time-dev valgrind cmake
 # export PATH=/usr/local/bin:$PATH
+sudo rm -r /usr/local/cmake-12.4/
 echo "Test"
+ls /usr/local/cmake-12.4/bin
 apt-cache madison cmake
 cmake --version
 
