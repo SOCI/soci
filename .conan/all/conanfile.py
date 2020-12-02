cda01f6585d3c83a1a1071e5488cd336b7edf9cb
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-12-01 19:03:22 -0500

    change source to get the version

--- .conan/all/conanfile.py
+++ .conan/all/conanfile.py
@@ -1,3 +1,5 @@
+import os
+
 from conans import ConanFile, CMake, tools
 
 class SociConan(ConanFile):
@@ -40,6 +42,16 @@ class SociConan(ConanFile):
         "postgresql": False
     }
 
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
     def requirements(self):
         if self.options.sqlite3:
             self.requires("sqlite3/3.33.0")
@@ -47,32 +59,46 @@ class SociConan(ConanFile):
         # ToDo add the dependencies for the missing backends
 
     def source(self):
-        self.run("git clone https://github.com/SOCI/soci.git")
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename(self.name + "-" + self.version, self._source_subfolder)
 
-    def build(self):
-        cmake = CMake(self)
-        cmake.definitions["SOCI_SHARED"]     = self.options.shared
-        cmake.definitions["SOCI_CXX11"]      = self.options.cxx11
-        cmake.definitions["SOCI_SQLITE3"]    = self.options.sqlite3
-        cmake.definitions["SOCI_EMPTY"]      = self.options.empty
-        cmake.definitions["SOCI_TESTS"]      = self.options.tests
-        cmake.definitions["SOCI_STATIC"]     = self.options.static
-        cmake.definitions["SOCI_DB2"]        = self.options.db2
-        cmake.definitions["SOCI_ODBC"]       = self.options.odbc
-        cmake.definitions["SOCI_ORACLE"]     = self.options.oracle
-        cmake.definitions["SOCI_FIREBIRD"]   = self.options.firebird
-        cmake.definitions["SOCI_MYSQL"]      = self.options.mysql
-        cmake.definitions["SOCI_POSTGRESQL"] = self.options.postgresql
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+
+        self._cmake = CMake(self)
+
+        self._cmake.definitions["SOCI_SHARED"]     = self.options.shared
+        self._cmake.definitions["SOCI_CXX11"]      = self.options.cxx11
+        self._cmake.definitions["SOCI_SQLITE3"]    = self.options.sqlite3
+        self._cmake.definitions["SOCI_EMPTY"]      = self.options.empty
+        self._cmake.definitions["SOCI_TESTS"]      = self.options.tests
+        self._cmake.definitions["SOCI_STATIC"]     = self.options.static
+        self._cmake.definitions["SOCI_DB2"]        = self.options.db2
+        self._cmake.definitions["SOCI_ODBC"]       = self.options.odbc
+        self._cmake.definitions["SOCI_ORACLE"]     = self.options.oracle
+        self._cmake.definitions["SOCI_FIREBIRD"]   = self.options.firebird
+        self._cmake.definitions["SOCI_MYSQL"]      = self.options.mysql
+        self._cmake.definitions["SOCI_POSTGRESQL"] = self.options.postgresql
 
         if self.options.cxx11:
-            cmake.definitions["CMAKE_CXX_STANDARD"] = "11"
+            self._cmake.definitions["CMAKE_CXX_STANDARD"] = "11"
 
-        cmake.configure(source_folder="soci")
+        self._cmake.configure(
+            source_folder=self._source_subfolder,
+            build_folder=self._build_subfolder
+        )
+
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
         cmake.install()
 
     def package(self):
-        self.copy("*.h", dst="include", src="soci/include")
+        include_folder = os.path.join(self._source_subfolder, "include")
+        self.copy("*.h", dst="include", src=include_folder)
         self.copy("*soci*.lib", dst="lib", src="lib", keep_path=False, symlinks=True)
         self.copy("*soci*.so*", dst="lib", src="lib", keep_path=False, symlinks=True)
         self.copy("*.dylib", dst="lib", keep_path=False, symlinks=True)
