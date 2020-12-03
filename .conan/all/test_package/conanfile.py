750db0d5944ae2504b7815e7269fe50333d5699e
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-12-02 19:34:59 -0500

    add fmt and catch2
    - Run the tests instead

--- .conan/all/test_package/conanfile.py
+++ .conan/all/test_package/conanfile.py
@@ -5,11 +5,22 @@ from conans import ConanFile, CMake, tools
 class SociTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    requires = ["catch2/2.13.3", "fmt/6.2.0"]
+
+    def requirements(self):
+        self.requires("catch2/2.13.3")
+        self.requires("fmt/6.2.0")
 
     def configure(self):
-        self.options["soci"].cxx11   = True
-        self.options["soci"].sqlite3 = True
-        self.options["soci"].empty   = True
+        self.options["soci"].cxx11      = True
+        self.options["soci"].empty      = False
+        self.options["soci"].sqlite3    = True
+        self.options["soci"].db2        = False
+        self.options["soci"].odbc       = False
+        self.options["soci"].oracle     = False
+        self.options["soci"].firebird   = False
+        self.options["soci"].mysql      = False
+        self.options["soci"].postgresql = False
 
     def build(self):
         cmake = CMake(self)
@@ -18,4 +29,5 @@ class SociTestConan(ConanFile):
 
     def test(self):
         if not tools.cross_building(self):
-            self.run(".%sexample" % os.sep)
+            # self.run(".%sexample" % os.sep)
+            self.run("ctest . -VV")
