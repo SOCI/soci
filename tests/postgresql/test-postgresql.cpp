SOCI/soci

commit 99d2606375131a5bc741972e1babd84d4ce69aeb

Fix handling of identifiers with colons in PostgreSQL backend
Colon inside identifiers was still interpreted specially by SOCI, even
when they were quoted.

Fix this by suppressing the colon special meaning inside quotes.

Closes https://github.com/SOCI/soci/pull/782

src/backends/postgresql/statement.cpp
@@ -107,7 +107,7 @@ void postgresql_statement_backend::prepare(std::string const & query,
     // rewrite the query by transforming all named parameters into
     // the postgresql_ numbers ones (:abc -> $1, etc.)
 
-    enum { normal, in_quotes, in_name } state = normal;
+    enum { normal, in_quotes, in_identifier, in_name } state = normal;
 
     std::string name;
     int position = 1;
@@ -122,6 +122,11 @@ void postgresql_statement_backend::prepare(std::string const & query,
             {
                 query_ += *it;
                 state = in_quotes;
+            }
+            else if (*it == '\"')
+            {
+                query_ += *it;
+                state = in_identifier;
             }
             else if (*it == ':')
             {
@@ -151,7 +156,8 @@ void postgresql_statement_backend::prepare(std::string const & query,
             }
             break;
         case in_quotes:
-            if (*it == '\'')
+        case in_identifier:
+            if (*it == '\'' || *it == '\"' )
             {
                 query_ += *it;
                 state = normal;

tests/postgresql/test-postgresql.cpp
@@ -990,6 +990,67 @@ TEST_CASE("Bulk iterators", "[postgresql][bulkiters]")
     sql << "drop table t";
 }
 
+
+// false_bind_variable_inside_identifier
+struct test_false_bind_variable_inside_identifier_table_creator : table_creator_base
+{
+    test_false_bind_variable_inside_identifier_table_creator(session & sql)
+        : table_creator_base(sql)
+        , msession(sql) 
+    {
+
+        try
+        {
+            sql << "CREATE TABLE soci_test( \"column_with:colon\" integer)";
+            sql << "CREATE TYPE \"type_with:colon\" AS ENUM ('en_one', 'en_two');";
+            sql <<  "CREATE FUNCTION \"function_with:colon\"() RETURNS integer LANGUAGE 'sql' AS "
+                    "$BODY$"
+                    "   SELECT \"column_with:colon\" FROM soci_test LIMIT 1; "
+                    "$BODY$;"
+            ;
+        }
+        catch(...)
+        {
+            drop();
+        }
+        
+    }
+    ~test_false_bind_variable_inside_identifier_table_creator(){
+        drop();
+    }
+private:
+    void drop()
+    {
+        try
+        {
+            msession << "DROP FUNCTION IF EXISTS \"function_with:colon\"();";
+            msession << "DROP TYPE IF EXISTS \"type_with:colon\" ;";
+        }
+        catch (soci_error const& e){}
+    }
+    session& msession;
+};
+TEST_CASE("false_bind_variable_inside_identifier", "[postgresql][bind-variables]")
+{
+    std::string col_name;
+    int fct_return_value;
+    std::string type_value;
+
+    {
+        session sql(backEnd, connectString);
+        test_false_bind_variable_inside_identifier_table_creator tableCreator(sql);
+
+        sql << "insert into soci_test(\"column_with:colon\") values(2020)";
+        sql << "SELECT column_name FROM information_schema.columns WHERE table_schema = current_schema() AND table_name = 'soci_test';", into(col_name);
+        sql << "SELECT \"function_with:colon\"() ;", into(fct_return_value);
+        sql << "SELECT unnest(enum_range(NULL::\"type_with:colon\"))  ORDER BY 1 LIMIT 1;", into(type_value);
+    }
+
+    CHECK(col_name.compare("column_with:colon") == 0);
+    CHECK(fct_return_value == 2020);
+    CHECK(type_value.compare("en_one")==0);
+}
+
 //
 // Support for soci Common Tests
 //

commit 7ee01afc4c97cdc0a9be5924ab7c892b3e092295

Fix use of quoted identifiers with colons in PostgreSQL queries
This was broken by 99d26063 (Fix handling of identifiers with colons in
PostgreSQL backend, 2020-01-27), see #782.

Fix this and add a test checking for the use of such identifiers.

Closes #840.

src/backends/postgresql/statement.cpp
@@ -156,8 +156,18 @@ void postgresql_statement_backend::prepare(std::string const & query,
             }
             break;
         case in_quotes:
+           if (*it == '\'' )
+            {
+                query_ += *it;
+                state = normal;
+            }
+            else // regular quoted character
+            {
+                query_ += *it;
+            }
+            break;
         case in_identifier:
-            if (*it == '\'' || *it == '\"' )
+            if ( *it == '\"' )
             {
                 query_ += *it;
                 state = normal;

tests/postgresql/test-postgresql.cpp
@@ -1051,6 +1051,36 @@ TEST_CASE("false_bind_variable_inside_identifier", "[postgresql][bind-variables]
     CHECK(type_value.compare("en_one")==0);
 }
 
+// false_bind_variable_inside_identifier
+struct table_creator_colon_in_double_quotes_in_single_quotes :
+    table_creator_base
+{
+    table_creator_colon_in_double_quotes_in_single_quotes(session & sql)
+        : table_creator_base(sql)
+    {
+       sql << "CREATE TABLE soci_test( \"column_with:colon\" text)";
+    }
+
+};
+TEST_CASE("colon_in_double_quotes_in_single_quotes",
+          "[postgresql][bind-variables]")
+{
+    std::string return_value;
+
+    {
+        session sql(backEnd, connectString);
+        table_creator_colon_in_double_quotes_in_single_quotes
+            tableCreator(sql);
+
+        sql << "insert into soci_test(\"column_with:colon\") values('hello "
+               "it is \"10:10\"')";
+        sql << "SELECT \"column_with:colon\" from soci_test ;", into
+            (return_value);
+    }
+
+    CHECK(return_value == "hello it is \"10:10\"");
+}
+
 //
 // Support for soci Common Tests
 //