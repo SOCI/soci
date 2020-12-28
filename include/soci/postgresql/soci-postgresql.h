d2c20077c138fe781d92b0809cf8f7f10d22648a
nobodyxu <Jiahao_XU@outlook.com>
2020-12-28 14:11:54 +1100

    Use SOCI_DECL_{EXPORT|IMPORT} to def SOCI_POSTGRESQL_DECL

--- include/soci/postgresql/soci-postgresql.h
+++ include/soci/postgresql/soci-postgresql.h
@@ -9,23 +9,12 @@
 #ifndef SOCI_POSTGRESQL_H_INCLUDED
 #define SOCI_POSTGRESQL_H_INCLUDED
 
-#ifdef _WIN32
-# ifdef SOCI_DLL
-#  ifdef SOCI_POSTGRESQL_SOURCE
-#   define SOCI_POSTGRESQL_DECL __declspec(dllexport)
-#  else
-#   define SOCI_POSTGRESQL_DECL __declspec(dllimport)
-#  endif // SOCI_POSTGRESQL_SOURCE
-# endif // SOCI_DLL
-#endif // _WIN32
-//
-// If SOCI_POSTGRESQL_DECL isn't defined yet define it now
-#ifndef SOCI_POSTGRESQL_DECL
-# if __GNUC__ >= 4
-#  define SOCI_POSTGRESQL_DECL __attribute__ ((visibility ("default")))
-# else
-#  define SOCI_POSTGRESQL_DECL
-# endif
+#include "soci/soci-platform.h"
+
+#ifdef SOCI_POSTGRESQL_SOURCE
+# define SOCI_POSTGRESQL_DECL SOCI_DECL_EXPORT
+#else
+# define SOCI_POSTGRESQL_DECL SOCI_DECL_IMPORT
 #endif
 
 #include <soci/soci-backend.h>
