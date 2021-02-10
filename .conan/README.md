e72011f893ca31d519b5498f843321909fc3b47d
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-02-10 17:42:00 -0500

    update recipe to the one accepted by conan
    - Update readme steps

--- .conan/README.md
+++ .conan/README.md
@@ -5,66 +5,104 @@ How to test the package from the path (`... soci/.conan/all`):
 
     conan create . <version>@
     conan create . 4.0.1@
-    conan create . 4.0.0@
 
-If the package creation was successful it will also run the tests for `sqlite3`
+This recipe is meant to be tested only locally.
 
 ## How to use it
-This recipe is meant to be tested only locally, but 
-once the recipe is added to [conan.io/center](https://conan.io/center/),
-the way to include SOCI in a new project using `cmake` will
-be the following:
 
-1. Create a `conanfile.txt` using `sqlite3` as backend: 
-    
+The recipe can be found in [conan.io/center](https://conan.io/center/) for
+version `4.0.1`  
+
+1. There are two ways to include `SOCI` in a new `cmake` project:
+with a plain `conanfile.txt` or with a python `conanfile.py`.  
+
+    1.1 Create a plain `conanfile.txt` to use `soci` with `sqlite3` as backend:  
     ```
     [requires]
-    sqlite3/3.33.0
-    soci/4.0.1
+      soci/4.0.1
     
     [options]
-    soci:cxx11   = True
-    soci:sqlite3 = True
-    soci:empty   = True
+      soci:shared       = True
+      soci:with_sqlite3 = True
     
     [generators]
-    cmake
+      cmake
     ```
 
-2. Add the following lines to the `CMakeLists.txt`:
+    1.2. Create a `conanfile.py` to use `soci` with `sqlite3` as backend:
 
+    ```python
+    import os
+    from conans import ConanFile, CMake, tools
+    
+    class SomeProject(ConanFile):
+        settings = "os", "compiler", "build_type", "arch"
+        generators = "cmake"
+    
+        def configure(self):
+            self.options["soci"].shared         = True
+            self.options["soci"].with_sqlite3   = True
+    
+        def build(self):
+            cmake = CMake(self)
+            cmake.configure()
+            cmake.build()
     ```
+
+2. Add the following lines to the `CMakeLists.txt`:  
+    ```cmake
     include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
     conan_basic_setup(TARGETS)
     conan_target_link_libraries(${PROJECT_NAME} ${CONAN_LIBS})
     ```
 
-    The command `conan_target_link_libraries()` will replace the `target_link_libraries()`, so
+    - The command `conan_target_link_libraries()` will replace the `target_link_libraries()`, so
     if you have any other library to link you can add it here instead, and remove
     the `target_link_libraries()`  
-    The `${PROJECT_NAME}` is the name of the target.
+    - The `${PROJECT_NAME}` is the name of the target.
+    - The `${CONAN_LIBS}` is the collection of library names to be included. They
+    can also be listed individually (`CONAN_PKG::soci_core`, `CONAN_PKG::sqlite3`)
 
-3. Run from your build directory.  
-    `conan install <path_to>/conanfile.txt` 
+3. Run the conan command from the build directory:  
+    `conan install .. --build=soci` 
 
-4. Build your project.
+4. Include the required headers:  
+    ```c++
+    #include "soci/soci.h"
+    #include "soci/sqlite3/soci-sqlite3.h"
+    #include "sqlite3.h"
+    ```
+
+5. Configure and build your project as usual:  
+    `cmake ..`  
+    `cmake --build .`  
+
+That's it.
 
 ## Notes:
 
-- To install conan use `python > 3`: `pip3 install --user conan`  
+- To install conan use `python3`: `pip3 install --user conan`  
 - Conan version used: `1.31.4`
 - Package tested on: `Ubuntu 18.04`, `gcc-7`
-- This Conan package supports `soci version > 4.0.0`
-- This Conan package has these backends configured:
-    - sqlite3
+- Requires `soci version=4.0.1`
+- Requires `cpp_standard >= 11`
+- Backends configured:
+    - [x] sqlite3
+    - [x] odbc
+    - [x] mysql
+    - [x] postgresql
+    - [ ] DB2
+    - [ ] Oracle
+    - [ ] firebird
+
 
 ## Dependencies summary
  
 |Option     | Library generated | has dependencies  | 
 |:---:      | ---               | ---               |
 |shared     | soci_core         | no                |
-|empty      | soci_empty        | no                |
 |static     |   ??              | ??                |
+|empty      | soci_empty        | no                |
 |cxx11      |   --              | y: `cxx_std > 11`       |
 |sqlite3    | soci_sqlite       | y: `sqlite3 v 3.33.0`  |
 |db2        |   N/A             | yes               |
@@ -73,4 +111,3 @@ be the following:
 |firebird   |   N/A             | yes               |
 |mysql      | soci_mysql        | yes               |
 |postgresql | soci_postgresql   | yes               |
-|tests      |   ??              | ??                |
--- .conan/all/conanfile.py
+++ .conan/all/conanfile.py
@@ -1,85 +1,109 @@
 import os
-
 from conans import ConanFile, CMake, tools
+from conans.tools import Version
+from conans.errors import ConanInvalidConfiguration, ConanException
 
 class SociConan(ConanFile):
     name = "soci"
     homepage = "https://github.com/SOCI/soci"
     url = "https://github.com/conan-io/conan-center-index"
     description = "The C++ Database Access Library "
-    topics = ("C++", "database-library", "oracle", "postgresql", "mysql", "odbc", "db2", "firebird", "sqlite3", "boost" )
-    license = "Boost"
+    topics = ("cpp", "database-library", "oracle", "postgresql", "mysql", "odbc", "db2", "firebird", "sqlite3", "boost" )
+    license = "BSL-1.0"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    exports_sources = ["CMakeLists.txt"]
+    _cmake = None
 
     options = {
-        "fPIC":       [True, False],
-        "shared":     [True, False],
-        "cxx11":      [True, False],
-        "sqlite3":    [True, False],
-        "empty":      [True, False],
-        "tests":      [True, False],
-        "static":     [True, False],
-        "db2":        [True, False],
-        "odbc":       [True, False],
-        "oracle":     [True, False],
-        "firebird":   [True, False],
-        "mysql":      [True, False],
-        "postgresql": [True, False]
-    }
-    default_options = {
-        "fPIC":       True,
-        "shared":     False,
-        "cxx11":      False,
-        "sqlite3":    False,
-        "empty":      False,
-        "tests":      False,
-        "static":     False,
-        "db2":        False,
-        "odbc":       False,
-        "oracle":     False,
-        "firebird":   False,
-        "mysql":      False,
-        "postgresql": False
+        "fPIC":             [True, False],
+        "shared":           [True, False],
+        "empty":            [True, False],
+        "with_sqlite3":     [True, False],
+        "with_db2":         [True, False],
+        "with_odbc":        [True, False],
+        "with_oracle":      [True, False],
+        "with_firebird":    [True, False],
+        "with_mysql":       [True, False],
+        "with_postgresql":  [True, False],
+        "with_boost":       [True, False]
     }
 
-    _cmake = None
+    default_options = {
+        "fPIC":             True,
+        "shared":           False,
+        "empty":            False,
+        "with_sqlite3":     False,
+        "with_db2":         False,
+        "with_odbc":        False,
+        "with_oracle":      False,
+        "with_firebird":    False,
+        "with_mysql":       False,
+        "with_postgresql":  False,
+        "with_boost":       False
+    }
 
     @property
     def _source_subfolder(self):
         return "source_subfolder"
 
     @property
-    def _build_subfolder(self):
-        return "build_subfolder"
+    def _minimum_cpp_standard(self):
+        return 11
 
-    def requirements(self):
-        prefix  = "Dependencies for "
-        message = " not configured in this conan package, some features will be disabled."
-
-        if self.options.sqlite3:
-            self.requires("sqlite3/3.33.0")
-        # ToDo add the missing dependencies for the backends
-        if self.options.db2:
-            self.output.warn(prefix + "DB2" + message)
-        if self.options.odbc:
-            self.output.warn(prefix + "ODBC" + message)
-        if self.options.oracle:
-            self.output.warn(prefix + "ORACLE" + message)
-        if self.options.firebird:
-            self.output.warn(prefix + "FIREBIRD" + message)
-        if self.options.mysql:
-            self.output.warn(prefix + "MYSQL" + message)
-        if self.options.postgresql:
-            self.output.warn(prefix + "POSTGRESQL" + message)
-
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        os.rename(self.name + "-" + self.version, self._source_subfolder)
+    @property
+    def _minimum_compilers_version(self):
+        return {
+            "Visual Studio": "14",
+            "gcc": "4.8",
+            "clang": "3.8",
+            "apple-clang": "8.0"
+        }
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+        compiler = str(self.settings.compiler)
+        compiler_version = Version(self.settings.compiler.version.value)
+        tools.check_min_cppstd(self, self._minimum_cpp_standard)
+
+        if compiler not in self._minimum_compilers_version:
+            self.output.warn("{} recipe lacks information about the {} compiler support.".format(self.name, self.settings.compiler))
+        elif compiler_version < self._minimum_compilers_version[compiler]:
+            raise ConanInvalidConfiguration("{} requires a {} version >= {}".format(self.name, compiler, compiler_version))
+
+    def requirements(self):
+        prefix  = "Dependencies for"
+        message = "not configured in this conan package."
+
+        if self.options.with_sqlite3:
+            self.requires("sqlite3/3.33.0")
+        if self.options.with_db2:
+            # self.requires("db2/0.0.0") # TODO add support for db2
+            raise ConanInvalidConfiguration("{} DB2 {} ".format(prefix, message))
+        if self.options.with_odbc:
+            self.requires("odbc/2.3.7")
+        if self.options.with_oracle:
+            # self.requires("oracle_db/0.0.0") # TODO add support for oracle
+            raise ConanInvalidConfiguration("{} ORACLE {} ".format(prefix, message))
+        if self.options.with_firebird:
+            # self.requires("firebird/0.0.0") # TODO add support for firebird
+            raise ConanInvalidConfiguration("{} firebird {} ".format(prefix, message))
+        if self.options.with_mysql:
+            self.requires("libmysqlclient/8.0.17")
+        if self.options.with_postgresql:
+            self.requires("libpq/11.5")
+        if self.options.with_boost:
+            self.requires("boost/1.73.0")
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename(self.name + "-" + self.version, self._source_subfolder)
 
     def _configure_cmake(self):
         if self._cmake:
@@ -87,60 +111,70 @@ class SociConan(ConanFile):
 
         self._cmake = CMake(self)
 
-        self._cmake.definitions["SOCI_SHARED"]     = self.options.shared
-        self._cmake.definitions["SOCI_STATIC"]     = self.options.static
-        self._cmake.definitions["SOCI_EMPTY"]      = self.options.empty
-        self._cmake.definitions["SOCI_TESTS"]      = self.options.tests
-        self._cmake.definitions["SOCI_CXX11"]      = self.options.cxx11
-        self._cmake.definitions["SOCI_SQLITE3"]    = self.options.sqlite3
-        self._cmake.definitions["SOCI_DB2"]        = self.options.db2
-        self._cmake.definitions["SOCI_ODBC"]       = self.options.odbc
-        self._cmake.definitions["SOCI_ORACLE"]     = self.options.oracle
-        self._cmake.definitions["SOCI_FIREBIRD"]   = self.options.firebird
-        self._cmake.definitions["SOCI_MYSQL"]      = self.options.mysql
-        self._cmake.definitions["SOCI_POSTGRESQL"] = self.options.postgresql
+        self._cmake.definitions["SOCI_SHARED"]  = self.options.shared
+        self._cmake.definitions["SOCI_TESTS"]   = False
+        self._cmake.definitions["SOCI_CXX11"]   = True
 
-        if self.options.cxx11:
-            self._cmake.definitions["CMAKE_CXX_STANDARD"] = "11" # ToDo review this. The standard should not be set here
+        if self.options.shared:
+            self._cmake.definitions["SOCI_STATIC"] = False
 
-        self._cmake.configure(
-            source_folder=self._source_subfolder,
-            build_folder=self._build_subfolder
-        )
+        self._cmake.definitions["SOCI_EMPTY"]       = self.options.empty
+        self._cmake.definitions["WITH_SQLITE3"]     = self.options.with_sqlite3
+        self._cmake.definitions["WITH_DB2"]         = self.options.with_db2
+        self._cmake.definitions["WITH_ODBC"]        = self.options.with_odbc
+        self._cmake.definitions["WITH_ORACLE"]      = self.options.with_oracle
+        self._cmake.definitions["WITH_FIREBIRD"]    = self.options.with_firebird
+        self._cmake.definitions["WITH_MYSQL"]       = self.options.with_mysql
+        self._cmake.definitions["WITH_POSTGRESQL"]  = self.options.with_postgresql
+        self._cmake.definitions["WITH_BOOST"]       = self.options.with_boost
+
+        self._cmake.configure()
 
         return self._cmake
 
     def build(self):
         cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        include_folder  = os.path.join(self._source_subfolder, "include")
-        lib_folder      = os.path.join(self._build_subfolder, "lib")
-        bin_folder      = os.path.join(self._build_subfolder, "bin")
-
-        self.copy("*.h",    dst="include", src=include_folder)
-        self.copy("*soci*.lib", dst="lib", src=lib_folder, keep_path=False, symlinks=True)
-        self.copy("*soci*.so*", dst="lib", src=lib_folder, keep_path=False, symlinks=True)
-        self.copy("*.a",        dst="lib", src=lib_folder, keep_path=False, symlinks=True)
-        self.copy("*.dylib",    dst="lib", src=lib_folder, keep_path=False, symlinks=True)
-        self.copy("*.dll",      dst="bin", src=bin_folder, keep_path=False, symlinks=True)
         self.copy("LICENSE_1_0.txt", dst="licenses", src=self._source_subfolder)
 
-    def package_info(self):
-        self.cpp_info.includedirs = ['include']
-        self.cpp_info.libdirs = ['lib', 'lib64']
-        self.cpp_info.builddirs = ['cmake']
+        cmake = self._configure_cmake()
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "cmake"))
 
+        if os.path.isdir(os.path.join(self.package_folder, "lib64")):
+            if os.path.isdir(os.path.join(self.package_folder, "lib")):
+                self.copy("*", dst="lib", src="lib64", keep_path=False, symlinks=True)
+                tools.rmdir(os.path.join(self.package_folder, "lib64"))
+            else:
+                tools.rename(os.path.join(self.package_folder, "lib64"), os.path.join(self.package_folder, "lib"))
+
+        os.remove(os.path.join(self.package_folder, "include", "soci", "soci-config.h.in"))
+
+    def package_info(self):
         self.cpp_info.libs = ["soci_core"]
         if self.options.empty:
             self.cpp_info.libs.append("soci_empty")
-        if self.options.sqlite3:
+        if self.options.with_sqlite3:
             self.cpp_info.libs.append("soci_sqlite3")
-        if self.options.oracle:
+        if self.options.with_oracle:
             self.cpp_info.libs.append("soci_oracle")
-        if self.options.mysql:
+        if self.options.with_mysql:
             self.cpp_info.libs.append("soci_mysql")
-        if self.options.postgresql:
+        if self.options.with_postgresql:
             self.cpp_info.libs.append("soci_postgresql")
+
+        if self.settings.os == "Windows":
+            for index, name in enumerate(self.cpp_info.libs):
+                self.cpp_info.libs[index] = self._rename_library_win(name)
+
+    def _rename_library_win(self, name):
+        if self.options.shared:
+            prefix = ""
+        else:
+            prefix = "lib"
+
+        abi_version = tools.Version(self.version)
+        sufix = "_{}_{}".format(abi_version.major, abi_version.minor)
+        return "{}{}{}".format(prefix, name, sufix)
