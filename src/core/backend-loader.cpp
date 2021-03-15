1b1b5621f5abc40bd76a54a779455e8b9c0892ff
Vadim Zeitlin <vz-soci@zeitlins.org>
2021-03-15 22:51:09 +0100

    Delay unloading of dynamic backends until they're no longer used
    Calling dynamic_backends::unload() or unload_all() could result in
    crashes if there were soci::session objects still using the backend
    being unloaded in the same program and this was difficult to avoid as
    this could be done by two different libraries not knowing about each
    other (but both using SOCI).
    
    Make calling unload() safer by not actually unloading the backend
    immediately if it's in use and doing it later, when its use count drops
    to 0.

--- src/core/backend-loader.cpp
+++ src/core/backend-loader.cpp
@@ -89,7 +89,25 @@ struct info
 {
     soci_handler_t handler_;
     backend_factory const * factory_;
-    info() : handler_(0), factory_(0) {}
+
+    // The use count is the number of existing sessions using this backend (in
+    // fact it's the count of connection_parameters objects, but as these
+    // objects are part of the session, it's roughly the same thing).
+    //
+    // While use count is non-zero, the corresponding backend can't be unloaded
+    // as this would leave the code using it with dangling (code) pointers. If
+    // it reaches 0, the backend is _not_ unloaded automatically because we
+    // don't want to unload/reload it all the time when recreating sessions,
+    // but it can be unloaded manually, if necessary, by calling unload() or
+    // unload_all() functions.
+    int use_count_;
+
+    // If unloading this backend is requested while its use count is non-zero,
+    // this flag is set to true and the backend is actually unloaded when the
+    // use count drops to 0.
+    bool unload_requested_;
+
+    info() : handler_(0), factory_(0), use_count_(0), unload_requested_(false) {}
 };
 
 typedef std::map<std::string, info> factory_map;
@@ -168,26 +186,42 @@ private:
     soci_mutex_t * mptr;
 };
 
-// non-synchronized helper for the other functions
-void do_unload(std::string const & name)
+// non-synchronized helpers for the other functions
+factory_map::iterator do_unload(factory_map::iterator i)
+{
+    soci_handler_t h = i->second.handler_;
+    if (h != NULL)
+    {
+        DLCLOSE(h);
+    }
+
+    // TODO-C++11: Use erase() return value.
+    factory_map::iterator const to_erase = i;
+    ++i;
+    factories_.erase(to_erase);
+    return i;
+}
+
+void do_unload_or_throw_if_in_use(std::string const & name)
 {
     factory_map::iterator i = factories_.find(name);
 
     if (i != factories_.end())
     {
-        soci_handler_t h = i->second.handler_;
-        if (h != NULL)
+        if (i->second.use_count_)
         {
-            DLCLOSE(h);
+            throw soci_error("Backend " + name + " is used and can't be unloaded");
         }
 
-        factories_.erase(i);
+        do_unload(i);
     }
 }
 
 // non-synchronized helper
 void do_register_backend(std::string const & name, std::string const & shared_object)
 {
+    do_unload_or_throw_if_in_use(name);
+
     // The rules for backend search are as follows:
     // - if the shared_object is given,
     //   it names the library file and the search paths are not used
@@ -238,10 +272,6 @@ void do_register_backend(std::string const & name, std::string const & shared_ob
         throw soci_error("Failed to resolve dynamic symbol: " + symbol);
     }
 
-    // unload the existing handler if it's already loaded
-
-    do_unload(name);
-
     backend_factory const* f = entry();
 
     info new_entry;
@@ -270,7 +300,36 @@ backend_factory const& dynamic_backends::get(std::string const& name)
       i = factories_.find(name);
     }
 
+    i->second.use_count_++;
+
     return *(i->second.factory_);
+}
+
+void dynamic_backends::unget(std::string const& name)
+{
+    scoped_lock lock(&mutex_);
+
+    factory_map::iterator i = factories_.find(name);
+
+    if (i == factories_.end())
+    {
+        // We don't throw here as this is often called from destructors, and so
+        // this would result in a call to std::terminate(), even if this is
+        // totally unexpected -- but, unfortunately, we have no way to report
+        // it to the application without possibly killing it.
+        return;
+    }
+
+    info& backend_info = i->second;
+
+    --backend_info.use_count_;
+
+    // Check if this backend should be unloaded if unloading it had been
+    // previously requested.
+    if (backend_info.use_count_ == 0 && backend_info.unload_requested_)
+    {
+        do_unload(i);
+    }
 }
 
 SOCI_DECL std::vector<std::string>& dynamic_backends::search_paths()
@@ -291,9 +350,7 @@ SOCI_DECL void dynamic_backends::register_backend(
 {
     scoped_lock lock(&mutex_);
 
-    // unload the existing handler if it's already loaded
-
-    do_unload(name);
+    do_unload_or_throw_if_in_use(name);
 
     info new_entry;
     new_entry.factory_ = &factory;
@@ -321,21 +378,39 @@ SOCI_DECL void dynamic_backends::unload(std::string const& name)
 {
     scoped_lock lock(&mutex_);
 
-    do_unload(name);
+    factory_map::iterator i = factories_.find(name);
+
+    if (i != factories_.end())
+    {
+        info& backend_info = i->second;
+        if (backend_info.use_count_)
+        {
+            // We can't unload the backend while it's in use, so do it later
+            // when it's not used any longer.
+            backend_info.unload_requested_ = true;
+            return;
+        }
+
+        do_unload(i);
+    }
 }
 
 SOCI_DECL void dynamic_backends::unload_all()
 {
     scoped_lock lock(&mutex_);
 
-    for (factory_map::iterator i = factories_.begin(); i != factories_.end(); ++i)
+    for (factory_map::iterator i = factories_.begin(); i != factories_.end(); )
     {
-        soci_handler_t h = i->second.handler_;
-        if (0 != h)
-        {
-            DLCLOSE(h);
-        }
-    }
+        info& backend_info = i->second;
 
-    factories_.clear();
+        // Same logic as in unload() above.
+        if (backend_info.use_count_)
+        {
+            backend_info.unload_requested_ = true;
+            ++i;
+            continue;
+        }
+
+        i = do_unload(i);
+    }
 }
--- src/core/connection-parameters.cpp
+++ src/core/connection-parameters.cpp
@@ -10,8 +10,6 @@
 #include "soci/soci-backend.h"
 #include "soci/backend-loader.h"
 
-using namespace soci;
-
 char const * soci::option_reconnect = "reconnect";
 
 char const * soci::option_true = "1";
@@ -28,7 +26,7 @@ void parseConnectString(std::string const & connectString,
     std::string::size_type const p = connectString.find(protocolSeparator);
     if (p == std::string::npos)
     {
-        throw soci_error("No backend name found in " + connectString);
+        throw soci::soci_error("No backend name found in " + connectString);
     }
 
     backendName = connectString.substr(0, p);
@@ -37,20 +35,65 @@ void parseConnectString(std::string const & connectString,
 
 } // namespace anonymous
 
+namespace soci
+{
+
+namespace details
+{
+
+// Reference-counted dynamic backend reference which is used to ensure that we
+// call dynamic_backends::unget() the same number of times as we call get().
+class dynamic_backend_ref
+{
+public:
+    explicit dynamic_backend_ref(std::string const & backendName)
+      : backendName_(backendName),
+        refCount_(1)
+    {
+    }
+
+    ~dynamic_backend_ref()
+    {
+        dynamic_backends::unget(backendName_);
+    }
+
+    void inc_ref() { ++refCount_; }
+    void dec_ref()
+    {
+        if (--refCount_ == 0)
+        {
+            delete this;
+        }
+    }
+
+private:
+    friend class soci::connection_parameters;
+
+    // Empty when using the user-provided factory only, otherwise contains the
+    // backend name when the factory was obtained from dynamic_backends.
+    std::string backendName_;
+
+    int refCount_;
+};
+
+} // namespace details
+
 connection_parameters::connection_parameters()
-    : factory_(NULL)
+    : factory_(NULL), backendRef_(NULL)
 {
 }
 
 connection_parameters::connection_parameters(backend_factory const & factory,
     std::string const & connectString)
-    : factory_(&factory), connectString_(connectString)
+    : factory_(&factory), connectString_(connectString), backendRef_(NULL)
 {
 }
 
 connection_parameters::connection_parameters(std::string const & backendName,
     std::string const & connectString)
-    : factory_(&dynamic_backends::get(backendName)), connectString_(connectString)
+    : factory_(&dynamic_backends::get(backendName)),
+      connectString_(connectString),
+      backendRef_(new details::dynamic_backend_ref(backendName))
 {
 }
 
@@ -63,4 +106,39 @@ connection_parameters::connection_parameters(std::string const & fullConnectStri
 
     factory_ = &dynamic_backends::get(backendName);
     connectString_ = connectString;
+    backendRef_ = new details::dynamic_backend_ref(backendName);
 }
+
+connection_parameters::connection_parameters(connection_parameters const& other)
+    : factory_(other.factory_),
+      connectString_(other.connectString_),
+      backendRef_(other.backendRef_),
+      options_(other.options_)
+{
+    if (backendRef_)
+        backendRef_->inc_ref();
+}
+
+connection_parameters& connection_parameters::operator=(connection_parameters const& other)
+{
+    // Order is important in case of self-assignment.
+    if (other.backendRef_)
+        other.backendRef_->inc_ref();
+    if (backendRef_)
+        backendRef_->dec_ref();
+
+    factory_ = other.factory_;
+    connectString_ = other.connectString_;
+    backendRef_ = other.backendRef_;
+    options_ = other.options_;
+
+    return *this;
+}
+
+connection_parameters::~connection_parameters()
+{
+    if (backendRef_)
+      backendRef_->dec_ref();
+}
+
+} // namespace soci
