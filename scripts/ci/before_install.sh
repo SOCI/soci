f9da35df1b084910c10dd90b4f917822fb5b0069
Vadim Zeitlin <vz-soci@zeitlins.org>
2021-03-16 17:31:31 +0100

    Don't install Boost packages unnecessarily
    We don't need them when Boost support is explicitly turned off.

--- scripts/ci/before_install.sh
+++ scripts/ci/before_install.sh
@@ -5,8 +5,13 @@
 #
 source ${SOCI_SOURCE_DIR}/scripts/ci/common.sh
 
+packages_to_install=libc6-dbg
+if [ "${WITH_BOOST}" != OFF ]; then
+    packages_to_install="$packages_to_install  libboost-dev libboost-date-time-dev"
+fi
+
 sudo apt-get update -qq -y
-sudo apt-get install -qq -y libc6-dbg libboost-dev libboost-date-time-dev
+sudo apt-get install -qq -y ${packages_to_install}
 
 before_install="${SOCI_SOURCE_DIR}/scripts/ci/before_install_${SOCI_CI_BACKEND}.sh"
 if [ -x ${before_install} ]; then
