8a0a86e063925f671c75dc05cc66af215f0ff3e9
Vadim Zeitlin <vz-soci@zeitlins.org>
2021-03-16 18:24:09 +0100

    Fix using MySQL with GitHub Actions
    Start the service explicitly as this is not done by default.
    
    Also explicitly use password with mysql in GitHub Actions environment,
    both for the root and for the current user.
    
    This wasn't necessary on Travis CI but is required in this environment,
    so specify the root password explicitly and also create the MySQL user
    (which is not hardcoded "travis" any longer) and record its password in
    ~/.my.cnf.

--- scripts/ci/before_build_mysql.sh
+++ scripts/ci/before_build_mysql.sh
@@ -5,6 +5,29 @@
 #
 source ${SOCI_SOURCE_DIR}/scripts/ci/common.sh
 
+SOCI_MYSQL_USER=$(id -un)
+SOCI_MYSQL_PASS='mypass'
+
+if [ -n "${SOCI_MYSQL_ROOT_PASSWORD}" ]; then
+    sudo systemctl start mysql.service
+    SOCI_MYSQL_PASSWORD_OPT="-p${SOCI_MYSQL_ROOT_PASSWORD}"
+    mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "CREATE USER '${SOCI_MYSQL_USER}' IDENTIFIED BY '${SOCI_MYSQL_PASS}';"
+    if [ ! -f $HOME/.my.cnf ]; then
+        cat > $HOME/.my.cnf <<EOF
+[mysql]
+user=${SOCI_MYSQL_USER}
+password=${SOCI_MYSQL_PASS}
+EOF
+        echo Created $HOME/.my.cnf file with password for "${SOCI_MYSQL_USER}":
+    else
+        echo Using existing $HOME/.my.cnf file:
+    fi
+    echo '---------------------------------- >8 --------------------------------------'
+    cat $HOME/.my.cnf | tr 'A-Za-z' 'N-ZA-Mn-za-m'
+    echo '---------------------------------- >8 --------------------------------------'
+    echo '(using ROT-13 to ensure the password is not stripped from the output)'
+fi
+
 mysql --version
-mysql -u root -e "CREATE DATABASE soci_test;"
-mysql -u root -e "GRANT ALL PRIVILEGES ON soci_test.* TO 'travis'@'%';";
+mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "CREATE DATABASE soci_test;"
+mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "GRANT ALL PRIVILEGES ON soci_test.* TO '${SOCI_MYSQL_USER}'@'%';";
