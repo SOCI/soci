4ae2d90c5f7e4b864e149a63364d68a15c7806b2
Ilya Sinitsyn <isinitsyn@tt-solutions.com>
2021-04-14 00:18:35 +0700

    Fix blob memory leak in the Firebird backend
    Don't allocate a new blob if we already have it, just reuse it.
    
    Closes #871.
    
    Co-Authored-By: Vadim Zeitlin <vz-soci@zeitlins.org>

--- src/backends/firebird/standard-use-type.cpp
+++ src/backends/firebird/standard-use-type.cpp
@@ -166,7 +166,10 @@ void firebird_standard_use_type_backend::exchangeData()
 
 void firebird_standard_use_type_backend::copy_to_blob(const std::string& in)
 {
-    blob_ = new firebird_blob_backend(statement_.session_);
+    if (blob_)
+        blob_->trim(0);
+    else
+        blob_ = new firebird_blob_backend(statement_.session_);
     blob_->append(in.c_str(), in.length());
     blob_->save();
     memcpy(buf_, &blob_->bid_, sizeof(blob_->bid_));
