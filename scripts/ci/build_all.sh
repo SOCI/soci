19ef5874b63b92bcb7364ce970388f76b7b44eda
Vadim Zeitlin <vz-soci@zeitlins.org>
2021-07-10 11:10:44 +0200

    Build all backends in CodeQL workflow

---
+++ scripts/ci/build_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+# Builds SOCI with all backends.
+#
+# Copyright (c) 2021 Vadim Zeitlin <vz-soci@zeitlins.org>
+#
+source ${SOCI_SOURCE_DIR}/scripts/ci/common.sh
+
+# We don't use the default options here, as we don't want to turn off all the
+# backends.
+cmake ${SOCI_COMMON_CMAKE_OPTIONS} \
+    ..
+
+run_make
