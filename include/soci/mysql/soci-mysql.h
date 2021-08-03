e67045d6015776c41d237f2ccac829a9697279d4
Mathäus Mendel <github@mathausmendel.com>
2021-08-03 20:51:53 -0300

    Fixing pull request #902
    The previous pull request was missing code to properly build and
    fix the intended issue

--- include/soci/mysql/soci-mysql.h
+++ include/soci/mysql/soci-mysql.h
@@ -22,6 +22,7 @@
 #include <winsock.h> // SOCKET
 #endif // _WIN32
 #include <mysql.h> // MySQL Client
+#include <errmsg.h> // MySQL Error codes
 #include <vector>
 
 
@@ -32,7 +33,7 @@ class SOCI_MYSQL_DECL mysql_soci_error : public soci_error
 {
 public:
     mysql_soci_error(std::string const & msg, int errNum)
-        : soci_error(msg), err_num_(errNum) {
+        : soci_error(msg), err_num_(errNum), cat_(unknown) {
             if(errNum == CR_CONNECTION_ERROR ||
                errNum == CR_CONN_HOST_ERROR ||
                errNum == CR_SERVER_GONE_ERROR ||
@@ -42,7 +43,10 @@ public:
             }
         }
 
+    error_category get_error_category() const SOCI_OVERRIDE { return cat_; }
+
     unsigned int err_num_;
+    error_category cat_;
 };
 
 struct mysql_statement_backend;
