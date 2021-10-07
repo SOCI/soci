8a23e534db936367a212200b3d22b5eb97f6363e
Ilya Sinitsyn <isinitsyn@tt-solutions.com>
2021-10-07 22:12:38 +0700

    Enable XXX-dbgsym repositories

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -10,6 +10,16 @@ if [ "${WITH_BOOST}" != OFF ]; then
     packages_to_install="$packages_to_install  libboost-dev libboost-date-time-dev"
 fi
 
+codename=$(lsb_release --codename --short)
+# Enable the `-dbgsym` repositories.
+echo "deb http://ddebs.ubuntu.com ${codename} main restricted universe multiverse
+deb http://ddebs.ubuntu.com ${codename}-updates main restricted universe multiverse" | \
+sudo tee --append /etc/apt/sources.list.d/ddebs.list >/dev/null
+
+# Import the debug symbol archive signing key from the Ubuntu server.
+# Note that this command works only on Ubuntu 18.04 LTS and newer.
+sudo apt-get install -qq -y ubuntu-dbgsym-keyring
+
 sudo apt-get update -qq -y
 sudo apt-get install -qq -y ${packages_to_install}
 
