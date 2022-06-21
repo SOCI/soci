0cc89c7c6ef5bd59e21b0e77d975c4c79e66edf3
Vadim Zeitlin <vz-soci@zeitlins.org>
2022-06-22 01:39:01 +0200

    Only run Linux-specific commands under Linux in install.sh
    No real changes, just make sure we don't run apt-get under non-Linux
    systems.
    
    This commit is best viewed ignoring whitespace-only changes.

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -5,23 +5,27 @@
 #
 source ${SOCI_SOURCE_DIR}/scripts/ci/common.sh
 
-packages_to_install="${SOCI_CI_PACKAGES} libc6-dbg"
-if [ "${WITH_BOOST}" != OFF ]; then
-    packages_to_install="$packages_to_install  libboost-dev libboost-date-time-dev"
-fi
+case "$(uname)" in
+    Linux)
+        packages_to_install="${SOCI_CI_PACKAGES} libc6-dbg"
+        if [ "${WITH_BOOST}" != OFF ]; then
+            packages_to_install="$packages_to_install  libboost-dev libboost-date-time-dev"
+        fi
 
-codename=$(lsb_release --codename --short)
-# Enable the `-dbgsym` repositories.
-echo "deb http://ddebs.ubuntu.com ${codename} main restricted universe multiverse
-deb http://ddebs.ubuntu.com ${codename}-updates main restricted universe multiverse" | \
-sudo tee --append /etc/apt/sources.list.d/ddebs.list >/dev/null
+        codename=$(lsb_release --codename --short)
+        # Enable the `-dbgsym` repositories.
+        echo "deb http://ddebs.ubuntu.com ${codename} main restricted universe multiverse
+        deb http://ddebs.ubuntu.com ${codename}-updates main restricted universe multiverse" | \
+        sudo tee --append /etc/apt/sources.list.d/ddebs.list >/dev/null
 
-# Import the debug symbol archive signing key from the Ubuntu server.
-# Note that this command works only on Ubuntu 18.04 LTS and newer.
-sudo apt-get install -qq -y ubuntu-dbgsym-keyring
+        # Import the debug symbol archive signing key from the Ubuntu server.
+        # Note that this command works only on Ubuntu 18.04 LTS and newer.
+        sudo apt-get install -qq -y ubuntu-dbgsym-keyring
 
-sudo apt-get update -qq -y
-sudo apt-get install -qq -y ${packages_to_install}
+        sudo apt-get update -qq -y
+        sudo apt-get install -qq -y ${packages_to_install}
+        ;;
+esac
 
 install_script="${SOCI_SOURCE_DIR}/scripts/ci/install_${SOCI_CI_BACKEND}.sh"
 if [ -x ${install_script} ]; then
