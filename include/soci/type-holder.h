b9944b0235caf1b0b6f5a6537c3b2d275ea738c0
Vadim Zeitlin <vadim@zeitlins.org>
2022-06-22 16:05:27 +0200

    Extract the changes of the last commit in a reusable function
    No real changes yet, just make it possible to reuse this function.

--- include/soci/type-holder.h
+++ include/soci/type-holder.h
@@ -19,6 +19,31 @@ namespace soci
 namespace details
 {
 
+// Returns U* as T*, if the dynamic type of the pointer is really T.
+//
+// This should be used instead of dynamic_cast<> because using it doesn't work
+// when using libc++ and ELF visibility together. Luckily, when we don't need
+// the full power of the cast, but only need to check if the types are the
+// same, it can be done by comparing their type info objects.
+//
+// This function does _not_ replace dynamic_cast<> in all cases and notably
+// doesn't allow the input pointer to be null.
+template <typename T, typename U>
+T* checked_ptr_cast(U* ptr)
+{
+    // Check if they're identical first, as an optimization, and then compare
+    // their names to make it actually work with libc++.
+    std::type_info const& ti_ptr = typeid(*ptr);
+    std::type_info const& ti_ret = typeid(T);
+
+    if (&ti_ptr != &ti_ret && std::strcmp(ti_ptr.name(), ti_ret.name()) != 0)
+    {
+        return NULL;
+    }
+
+    return static_cast<T*>(ptr);
+}
+
 // Base class holder + derived class type_holder for storing type data
 // instances in a container of holder objects
 template <typename T>
@@ -33,18 +58,9 @@ public:
     template<typename T>
     T get()
     {
-        // Using dynamic_cast<> doesn't work when using libc++ and ELF
-        // visibility together. Luckily, here we don't need the full power of
-        // the cast anyhow, as we only need to check if the types are the same,
-        // which can be done by comparing their type info objects: check if
-        // they're identical first, as an optimization, and then comparing
-        // their names to make it actually work with libc++.
-        std::type_info const& ti_this = typeid(*this);
-        std::type_info const& ti_type = typeid(type_holder<T>);
-        if (&ti_this == &ti_type
-                || std::strcmp(ti_this.name(), ti_type.name()) == 0)
+        type_holder<T>* p = checked_ptr_cast<type_holder<T> >(this);
+        if (p)
         {
-            type_holder<T>* p = static_cast<type_holder<T> *>(this);
             return p->template value<T>();
         }
         else
