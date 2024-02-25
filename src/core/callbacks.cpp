15645401efd80b373ca65a800ce6f50c232db71d
Robert Adam <dev@robert-adam.de>
2024-02-25 18:12:05 +0100

    Fixed Windows build

---
+++ src/core/callbacks.cpp
@@ -0,0 +1,16 @@
+#define SOCI_SOURCE
+
+#include "soci/callbacks.h"
+#include "soci/soci-platform.h"
+
+using namespace soci;
+
+failover_callback::~failover_callback() {}
+
+void failover_callback::started() {}
+
+void failover_callback::finished(session& /* sql */) {}
+
+void failover_callback::failed(bool& /* out */ /* retry */, std::string& /* out */ /* newTarget */) {}
+
+void failover_callback:: aborted() {}
