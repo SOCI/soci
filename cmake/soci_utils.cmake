35f0f923c5781425fe7e09befbf54fa1435fd44c
Robert Adam <dev@robert-adam.de>
2024-04-13 20:11:17 +0200

    More work on installation support

--- cmake/soci_utils.cmake
+++ cmake/soci_utils.cmake
@@ -49,3 +49,132 @@ function(soci_verify_parsed_arguments)
     endforeach()
   endif()
 endfunction()
+
+
+# Initialize variables populated by soci_public_dependency
+set(SOCI_DEPENDENCY_VARIABLES
+  "SOCI_DEPENDENCY_SOCI_COMPONENTS"
+  "SOCI_DEPENDENCY_NAMES"
+  "SOCI_DEPENDENCY_TARGETS"
+  "SOCI_DEPENDENCY_REQUIRED"
+  "SOCI_DEPENDENCY_MACRO_NAMES"
+  "SOCI_DEPENDENCY_COMPONENTS"
+)
+if (NOT DEFINED SOCI_UTILS_ALREADY_INCLUDED)
+  foreach(VAR_NAME IN LISTS SOCI_DEPENDENCY_VARIABLES)
+    set("${VAR_NAME}" "" CACHE INTERNAL "")
+  endforeach()
+endif()
+
+# This function declares a public dependency of a given target in such a way that
+# the dependency is automatically populated to a generated SOCI config file.
+#
+# Use as
+#   soci_public_dependency(
+#      NAME <name>
+#      DEP_TARGET <dep target>
+#      TARGET <target>
+#      [MACRO_NAMES <macro name> ...]
+#      [REQUIRED]
+#      [COMPONENTS <component> ...]
+#   )
+# where
+# - <name> is the name of the dependency (used for lookup via find_package)
+# - <dep target> is the name of the target that will be imported upon a
+#                successful find_package call
+# - <target> is the name of the ALIAS target to link the found dependency to
+# - <component> are the names of specific components of the dependency
+macro(soci_public_dependency)
+  set(FLAGS "REQUIRED")
+  set(ONE_VAL_OPTIONS "TARGET" "NAME")
+  set(MULTI_VAL_OPTIONS "COMPONENTS" "MACRO_NAMES" "DEP_TARGETS")
+
+  cmake_parse_arguments(PUBLIC_DEP "${FLAGS}" "${ONE_VAL_OPTIONS}" "${MULTI_VAL_OPTIONS}" ${ARGV})
+  soci_verify_parsed_arguments(
+    PREFIX "PUBLIC_DEP"
+    FUNCTION_NAME "soci_public_dependency"
+    REQUIRED "TARGET" "NAME" "DEP_TARGETS"
+  )
+
+  # Sanity checking
+  if (NOT TARGET "${PUBLIC_DEP_TARGET}")
+    message(FATAL_ERROR "Provided TARGET '${PUBLIC_DEP_TARGET}' does not exist")
+  endif()
+
+  get_target_property(UNDERLYING_TARGET "${PUBLIC_DEP_TARGET}" ALIASED_TARGET)
+  if (NOT UNDERLYING_TARGET)
+    message(FATAL_ERROR "Provided TARGET '${PUBLIC_DEP_TARGET}' is expected to be an ALIAS target")
+  endif()
+
+
+  # Bookkeeping
+  list(APPEND SOCI_DEPENDENCY_SOCI_COMPONENTS "${PUBLIC_DEP_TARGET}")
+  list(APPEND SOCI_DEPENDENCY_NAMES "${PUBLIC_DEP_NAME}")
+  list(JOIN PUBLIC_DEP_DEP_TARGETS "|" STORED_TARGETS)
+  list(APPEND SOCI_DEPENDENCY_TARGETS "${STORED_TARGETS}")
+  list(APPEND SOCI_DEPENDENCY_REQUIRED "${PUBLIC_DEP_REQUIRED}")
+  if (PUBLIC_DEP_MACRO_NAMES)
+    list(JOIN PUBLIC_DEP_MACRO_NAMES "|" STORED_MACROS)
+  else()
+    set(STORED_MACROS "")
+  endif()
+  list(APPEND SOCI_DEPENDENCY_MACRO_NAMES "${STORED_MACROS}")
+  if (PUBLIC_DEP_COMPONENTS)
+    list(JOIN PUBLIC_DEP_COMPONENTS "|" STORED_COMPONENTS)
+  else()
+    set(STORED_COMPONENTS "")
+  endif()
+  list(APPEND SOCI_DEPENDENCY_COMPONENTS "${STORED_COMPONENTS}")
+
+
+  foreach(VAR_NAME IN LISTS SOCI_DEPENDENCY_VARIABLES)
+    set("${VAR_NAME}" "${${VAR_NAME}}" CACHE INTERNAL "")
+  endforeach()
+
+
+  # Search for the package now
+  if (PUBLIC_DEP_REQUIRED)
+    set(REQUIRED "REQUIRED")
+  else()
+    set(REQUIRED "")
+  endif()
+
+  if (PUBLIC_DEP_COMPONENTS)
+    set(COMPONENTS COMPONENTS ${PUBLIC_DEP_COMPONENTS})
+  else()
+    set(COMPONENTS "")
+  endif()
+
+  set(SKIP_SEARCH ON)
+  foreach (TGT IN LISTS PUBLIC_DEP_DEP_TARGETS)
+    if (NOT TARGET "${TGT}")
+      set(SKIP_SEARCH OFF)
+    endif()
+  endforeach()
+
+  if (NOT SKIP_SEARCH)
+    find_package("${PUBLIC_DEP_NAME}" ${COMPONENTS} ${REQUIRED})
+  endif()
+
+  set(FOUND_ONE OFF)
+  foreach (TGT IN LISTS PUBLIC_DEP_DEP_TARGETS)
+    if (NOT TARGET "${TGT}")
+      if (FOUND_ONE)
+        message(FATAL_ERROR "Some but not all targets of a SOCI dependency have been found")
+      endif()
+      return()
+    endif()
+
+    set(FOUND_ONE ON)
+
+    target_link_libraries("${UNDERLYING_TARGET}" PUBLIC "$<BUILD_INTERFACE:${TGT}>")
+  endforeach()
+
+  foreach (MACRO_NAME IN LISTS PUBLIC_DEP_MACRO_NAMES)
+    # Note: We don't want these compile definitions to be exported to the install tree
+    # -> We put the logic of when they should be defined into the cmake config files
+    target_compile_definitions("${UNDERLYING_TARGET}" PUBLIC "$<BUILD_INTERFACE:${MACRO_NAME}>")
+  endforeach()
+endmacro()
+
+set(SOCI_UTILS_ALREADY_INCLUDED TRUE)
