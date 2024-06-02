6a968f1ef7b9d0fbcea8eee212d4a3fb40f45e0b
Robert Adam <dev@robert-adam.de>
2024-06-02 19:27:07 +0200

    Prevent compiler flags from propagating to superprojects

--- cmake/soci_compiler_options.cmake
+++ cmake/soci_compiler_options.cmake
@@ -28,7 +28,11 @@ endif()
 
 if (MSVC)
   # Configure warnings
-  target_compile_options(soci_compiler_interface INTERFACE "/W4" "/we4266")
+  target_compile_options(soci_compiler_interface
+    INTERFACE
+      "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:/W4>"
+      "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:/we4266>"
+  )
 
   if (SOCI_ENABLE_WERROR)
     target_compile_options(soci_compiler_interface INTERFACE "/WX")
@@ -45,21 +49,21 @@ else()
   if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang" OR "${CMAKE_CXX_COMPILER}" MATCHES "clang")
     if(SOCI_ASAN AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 3.1)
       # This can also be used to set a linker flag
-      target_link_libraries(soci_compiler_interface INTERFACE "-fsanitize=address")
-      target_compile_options(soci_compiler_interface INTERFACE "-fsanitize=address")
+      target_link_libraries(soci_compiler_interface INTERFACE "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-fsanitize=address>")
+      target_compile_options(soci_compiler_interface INTERFACE "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-fsanitize=address>")
     endif()
 
     set(SOCI_USE_STD_FLAGS ON)
   elseif (CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
     if (SOCI_ASAN AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 4.8)
       # This can also be used to set a linker flag
-      target_link_libraries(soci_compiler_interface INTERFACE "-fsanitize=address")
-      target_compile_options(soci_compiler_interface INTERFACE "-fsanitize=address")
+      target_link_libraries(soci_compiler_interface INTERFACE "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-fsanitize=address>")
+      target_compile_options(soci_compiler_interface INTERFACE "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-fsanitize=address>")
     endif()
 
     if (CMAKE_COMPILER_IS_GNUCXX)
       if (NOT (CMAKE_SYSTEM_NAME MATCHES "FreeBSD"))
-        target_compile_options(soci_compiler_interface INTERFACE "-Wno-variadic-macros")
+        target_compile_options(soci_compiler_interface INTERFACE "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wno-variadic-macros>")
       endif()
     endif()
 
@@ -71,17 +75,17 @@ else()
   if (SOCI_USE_STD_FLAGS)
     target_compile_options(soci_compiler_interface
       INTERFACE
-        "-pedantic"
-        "-Wno-error=parentheses"
-        "-Wall"
-        "-Wextra"
-        "-Wpointer-arith"
-        "-Wcast-align"
-        "-Wcast-qual"
-        "-Wfloat-equal"
-        "-Woverloaded-virtual"
-        "-Wredundant-decls"
-        "-Wno-long-long"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-pedantic>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wno-error=parentheses>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wall>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wextra>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wpointer-arith>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wcast-align>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wcast-qual>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wfloat-equal>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Woverloaded-virtual>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wredundant-decls>"
+        "$<$<BOOL:${PROJECT_IS_TOP_LEVEL}>:-Wno-long-long>"
     )
   endif()
 endif()
