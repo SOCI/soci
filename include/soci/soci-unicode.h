a06185deeca2dacd649618c09da947e3f21c27fd
Benjamin Oldenburg <benjamin.oldenburg@ordis.co.th>
2024-06-20 18:19:47 +0700

    Optimize UTF conversion functions and improve error handling

--- include/soci/soci-unicode.h
+++ include/soci/soci-unicode.h
@@ -17,7 +17,7 @@ namespace soci
   {
     /**
      * @brief Converts a UTF-8 encoded string to a UTF-16 encoded string.
-     * 
+     *
      * @param utf8 The UTF-8 encoded string.
      * @return std::u16string The UTF-16 encoded string.
      * @throws soci_error if the input string contains invalid UTF-8 encoding.
@@ -25,45 +25,65 @@ namespace soci
     inline std::u16string utf8_to_utf16(const std::string &utf8)
     {
       std::u16string utf16;
-      for (size_t i = 0; i < utf8.size();)
-      {
-        uint32_t cp = 0;
-        if ((utf8[i] & 0x80) == 0)
-        {
-          cp = utf8[i++];
-        }
-        else if ((utf8[i] & 0xE0) == 0xC0)
-        {
-          cp = (utf8[i++] & 0x1F) << 6;
-          cp |= (utf8[i++] & 0x3F);
-        }
-        else if ((utf8[i] & 0xF0) == 0xE0)
-        {
-          cp = (utf8[i++] & 0x0F) << 12;
-          cp |= (utf8[i++] & 0x3F) << 6;
-          cp |= (utf8[i++] & 0x3F);
-        }
-        else if ((utf8[i] & 0xF8) == 0xF0)
-        {
-          cp = (utf8[i++] & 0x07) << 18;
-          cp |= (utf8[i++] & 0x3F) << 12;
-          cp |= (utf8[i++] & 0x3F) << 6;
-          cp |= (utf8[i++] & 0x3F);
-        }
-        else
-        {
-          throw soci_error("Invalid UTF-8 encoding");
-        }
+      utf16.reserve(utf8.size());
 
-        if (cp <= 0xFFFF)
-        { // BMP character
-          utf16.push_back(static_cast<char16_t>(cp));
+      for (std::size_t i = 0; i < utf8.size();)
+      {
+        unsigned char c1 = static_cast<unsigned char>(utf8[i++]);
+
+        if (c1 < 0x80)
+        {
+          utf16.push_back(static_cast<char16_t>(c1));
+        }
+        else if ((c1 & 0xE0) == 0xC0)
+        {
+          if (i >= utf8.size())
+            throw soci_error("Invalid UTF-8 sequence");
+
+          unsigned char c2 = static_cast<unsigned char>(utf8[i++]);
+          if ((c2 & 0xC0) != 0x80)
+            throw soci_error("Invalid UTF-8 sequence");
+
+          utf16.push_back(static_cast<char16_t>(((c1 & 0x1F) << 6) | (c2 & 0x3F)));
+        }
+        else if ((c1 & 0xF0) == 0xE0)
+        {
+          if (i + 1 >= utf8.size())
+            throw soci_error("Invalid UTF-8 sequence");
+
+          unsigned char c2 = static_cast<unsigned char>(utf8[i++]);
+          unsigned char c3 = static_cast<unsigned char>(utf8[i++]);
+          if (((c2 & 0xC0) != 0x80) || ((c3 & 0xC0) != 0x80))
+            throw soci_error("Invalid UTF-8 sequence");
+
+          utf16.push_back(static_cast<char16_t>(((c1 & 0x0F) << 12) | ((c2 & 0x3F) << 6) | (c3 & 0x3F)));
+        }
+        else if ((c1 & 0xF8) == 0xF0)
+        {
+          if (i + 2 >= utf8.size())
+            throw soci_error("Invalid UTF-8 sequence");
+
+          unsigned char c2 = static_cast<unsigned char>(utf8[i++]);
+          unsigned char c3 = static_cast<unsigned char>(utf8[i++]);
+          unsigned char c4 = static_cast<unsigned char>(utf8[i++]);
+          if (((c2 & 0xC0) != 0x80) || ((c3 & 0xC0) != 0x80) || ((c4 & 0xC0) != 0x80))
+            throw soci_error("Invalid UTF-8 sequence");
+
+          uint32_t codepoint = ((c1 & 0x07) << 18) | ((c2 & 0x3F) << 12) | ((c3 & 0x3F) << 6) | (c4 & 0x3F);
+          if (codepoint <= 0xFFFF)
+          {
+            utf16.push_back(static_cast<char16_t>(codepoint));
+          }
+          else
+          {
+            codepoint -= 0x10000;
+            utf16.push_back(static_cast<char16_t>((codepoint >> 10) + 0xD800));
+            utf16.push_back(static_cast<char16_t>((codepoint & 0x3FF) + 0xDC00));
+          }
         }
         else
-        { // Supplementary character
-          cp -= 0x10000;
-          utf16.push_back(static_cast<char16_t>((cp >> 10) + 0xD800));
-          utf16.push_back(static_cast<char16_t>((cp & 0x3FF) + 0xDC00));
+        {
+          throw soci_error("Invalid UTF-8 sequence");
         }
       }
 
@@ -72,7 +92,7 @@ namespace soci
 
     /**
      * @brief Converts a UTF-16 encoded string to a UTF-8 encoded string.
-     * 
+     *
      * @param utf16 The UTF-16 encoded string.
      * @return std::string The UTF-8 encoded string.
      * @throws soci_error if the input string contains invalid UTF-16 encoding.
@@ -80,50 +100,41 @@ namespace soci
     inline std::string utf16_to_utf8(const std::u16string &utf16)
     {
       std::string utf8;
-      for (size_t i = 0; i < utf16.size();)
-      {
-        uint32_t cp = utf16[i++];
-        if ((cp >= 0xD800) && (cp <= 0xDBFF))
-        { // High surrogate
-          if (i < utf16.size())
-          {
-            uint32_t low = utf16[i++];
-            if (low >= 0xDC00 && low <= 0xDFFF)
-            { // Low surrogate
-              cp = ((cp - 0xD800) << 10) + (low - 0xDC00) + 0x10000;
-            }
-            else
-            {
-              throw soci_error("Invalid UTF-16 encoding");
-            }
-          }
-        }
+      utf8.reserve(utf16.size() * 3);
 
-        if (cp < 0x80)
+      for (std::size_t i = 0; i < utf16.size();)
+      {
+        char16_t c = utf16[i++];
+
+        if (c < 0x80)
         {
-          utf8.push_back(static_cast<char>(cp));
+          utf8.push_back(static_cast<char>(c));
         }
-        else if (cp < 0x800)
+        else if (c < 0x800)
         {
-          utf8.push_back(0xC0 | ((cp >> 6) & 0x1F));
-          utf8.push_back(0x80 | (cp & 0x3F));
+          utf8.push_back(static_cast<char>(0xC0 | ((c >> 6) & 0x1F)));
+          utf8.push_back(static_cast<char>(0x80 | (c & 0x3F)));
         }
-        else if (cp < 0x10000)
+        else if ((c >= 0xD800) && (c <= 0xDBFF))
         {
-          utf8.push_back(0xE0 | ((cp >> 12) & 0x0F));
-          utf8.push_back(0x80 | ((cp >> 6) & 0x3F));
-          utf8.push_back(0x80 | (cp & 0x3F));
-        }
-        else if (cp < 0x110000)
-        {
-          utf8.push_back(0xF0 | ((cp >> 18) & 0x07));
-          utf8.push_back(0x80 | ((cp >> 12) & 0x3F));
-          utf8.push_back(0x80 | ((cp >> 6) & 0x3F));
-          utf8.push_back(0x80 | (cp & 0x3F));
+          if (i >= utf16.size())
+            throw soci_error("Invalid UTF-16 sequence");
+
+          char16_t c2 = utf16[i++];
+          if ((c2 < 0xDC00) || (c2 > 0xDFFF))
+            throw soci_error("Invalid UTF-16 sequence");
+
+          uint32_t codepoint = (((c & 0x3FF) << 10) | (c2 & 0x3FF)) + 0x10000;
+          utf8.push_back(static_cast<char>(0xF0 | ((codepoint >> 18) & 0x07)));
+          utf8.push_back(static_cast<char>(0x80 | ((codepoint >> 12) & 0x3F)));
+          utf8.push_back(static_cast<char>(0x80 | ((codepoint >> 6) & 0x3F)));
+          utf8.push_back(static_cast<char>(0x80 | (codepoint & 0x3F)));
         }
         else
         {
-          throw soci_error("Invalid code point");
+          utf8.push_back(static_cast<char>(0xE0 | ((c >> 12) & 0x0F)));
+          utf8.push_back(static_cast<char>(0x80 | ((c >> 6) & 0x3F)));
+          utf8.push_back(static_cast<char>(0x80 | (c & 0x3F)));
         }
       }
 
@@ -132,7 +143,7 @@ namespace soci
 
     /**
      * @brief Converts a UTF-16 encoded string to a UTF-32 encoded string.
-     * 
+     *
      * @param utf16 The UTF-16 encoded string.
      * @return std::u32string The UTF-32 encoded string.
      * @throws soci_error if the input string contains invalid UTF-16 encoding.
@@ -140,32 +151,36 @@ namespace soci
     inline std::u32string utf16_to_utf32(const std::u16string &utf16)
     {
       std::u32string utf32;
-      for (size_t i = 0; i < utf16.size();)
+      utf32.reserve(utf16.size());
+
+      for (std::size_t i = 0; i < utf16.size();)
       {
-        uint32_t cp = utf16[i++];
-        if ((cp >= 0xD800) && (cp <= 0xDBFF))
+        char16_t c = utf16[i++];
+
+        if ((c >= 0xD800) && (c <= 0xDBFF))
         {
-          if (i < utf16.size())
-          {
-            uint32_t low = utf16[i++];
-            if ((low >= 0xDC00) && (low <= 0xDFFF))
-            {
-              cp = ((cp - 0xD800) << 10) + (low - 0xDC00) + 0x10000;
-            }
-            else
-            {
-              throw soci_error("Invalid UTF-16 encoding");
-            }
-          }
+          if (i >= utf16.size())
+            throw soci_error("Invalid UTF-16 sequence");
+
+          char16_t c2 = utf16[i++];
+          if ((c2 < 0xDC00) || (c2 > 0xDFFF))
+            throw soci_error("Invalid UTF-16 sequence");
+
+          uint32_t codepoint = (((c & 0x3FF) << 10) | (c2 & 0x3FF)) + 0x10000;
+          utf32.push_back(codepoint);
         }
-        utf32.push_back(cp);
+        else
+        {
+          utf32.push_back(static_cast<char32_t>(c));
+        }
       }
+
       return utf32;
     }
 
     /**
      * @brief Converts a UTF-32 encoded string to a UTF-16 encoded string.
-     * 
+     *
      * @param utf32 The UTF-32 encoded string.
      * @return std::u16string The UTF-16 encoded string.
      * @throws soci_error if the input string contains invalid UTF-32 code points.
@@ -173,29 +188,32 @@ namespace soci
     inline std::u16string utf32_to_utf16(const std::u32string &utf32)
     {
       std::u16string utf16;
-      for (uint32_t cp : utf32)
+      utf16.reserve(utf32.size() * 2);
+
+      for (char32_t codepoint : utf32)
       {
-        if (cp <= 0xFFFF)
+        if (codepoint <= 0xFFFF)
         {
-          utf16.push_back(static_cast<char16_t>(cp));
+          utf16.push_back(static_cast<char16_t>(codepoint));
         }
-        else if (cp <= 0x10FFFF)
+        else if (codepoint <= 0x10FFFF)
         {
-          cp -= 0x10000;
-          utf16.push_back(static_cast<char16_t>((cp >> 10) + 0xD800));
-          utf16.push_back(static_cast<char16_t>((cp & 0x3FF) + 0xDC00));
+          codepoint -= 0x10000;
+          utf16.push_back(static_cast<char16_t>((codepoint >> 10) + 0xD800));
+          utf16.push_back(static_cast<char16_t>((codepoint & 0x3FF) + 0xDC00));
         }
         else
         {
           throw soci_error("Invalid UTF-32 code point");
         }
       }
+
       return utf16;
     }
 
     /**
      * @brief Converts a UTF-8 encoded string to a UTF-32 encoded string.
-     * 
+     *
      * @param utf8 The UTF-8 encoded string.
      * @return std::u32string The UTF-32 encoded string.
      * @throws soci_error if the input string contains invalid UTF-8 encoding.
@@ -203,37 +221,56 @@ namespace soci
     inline std::u32string utf8_to_utf32(const std::string &utf8)
     {
       std::u32string utf32;
-      for (size_t i = 0; i < utf8.size();)
+      utf32.reserve(utf8.size());
+
+      for (std::size_t i = 0; i < utf8.size();)
       {
-        uint32_t cp = 0;
-        if ((utf8[i] & 0x80) == 0)
-        { // 1-byte sequence
-          cp = utf8[i++];
+        unsigned char c1 = static_cast<unsigned char>(utf8[i++]);
+
+        if (c1 < 0x80)
+        {
+          utf32.push_back(static_cast<char32_t>(c1));
         }
-        else if ((utf8[i] & 0xE0) == 0xC0)
-        { // 2-byte sequence
-          cp = (utf8[i++] & 0x1F) << 6;
-          cp |= (utf8[i++] & 0x3F);
+        else if ((c1 & 0xE0) == 0xC0)
+        {
+          if (i >= utf8.size())
+            throw soci_error("Invalid UTF-8 sequence");
+
+          unsigned char c2 = static_cast<unsigned char>(utf8[i++]);
+          if ((c2 & 0xC0) != 0x80)
+            throw soci_error("Invalid UTF-8 sequence");
+
+          utf32.push_back(static_cast<char32_t>(((c1 & 0x1F) << 6) | (c2 & 0x3F)));
         }
-        else if ((utf8[i] & 0xF0) == 0xE0)
-        { // 3-byte sequence
-          cp = (utf8[i++] & 0x0F) << 12;
-          cp |= (utf8[i++] & 0x3F) << 6;
-          cp |= (utf8[i++] & 0x3F);
+        else if ((c1 & 0xF0) == 0xE0)
+        {
+          if (i + 1 >= utf8.size())
+            throw soci_error("Invalid UTF-8 sequence");
+
+          unsigned char c2 = static_cast<unsigned char>(utf8[i++]);
+          unsigned char c3 = static_cast<unsigned char>(utf8[i++]);
+          if (((c2 & 0xC0) != 0x80) || ((c3 & 0xC0) != 0x80))
+            throw soci_error("Invalid UTF-8 sequence");
+
+          utf32.push_back(static_cast<char32_t>(((c1 & 0x0F) << 12) | ((c2 & 0x3F) << 6) | (c3 & 0x3F)));
         }
-        else if ((utf8[i] & 0xF8) == 0xF0)
-        { // 4-byte sequence
-          cp = (utf8[i++] & 0x07) << 18;
-          cp |= (utf8[i++] & 0x3F) << 12;
-          cp |= (utf8[i++] & 0x3F) << 6;
-          cp |= (utf8[i++] & 0x3F);
+        else if ((c1 & 0xF8) == 0xF0)
+        {
+          if (i + 2 >= utf8.size())
+            throw soci_error("Invalid UTF-8 sequence");
+
+          unsigned char c2 = static_cast<unsigned char>(utf8[i++]);
+          unsigned char c3 = static_cast<unsigned char>(utf8[i++]);
+          unsigned char c4 = static_cast<unsigned char>(utf8[i++]);
+          if (((c2 & 0xC0) != 0x80) || ((c3 & 0xC0) != 0x80) || ((c4 & 0xC0) != 0x80))
+            throw soci_error("Invalid UTF-8 sequence");
+
+          utf32.push_back(static_cast<char32_t>(((c1 & 0x07) << 18) | ((c2 & 0x3F) << 12) | ((c3 & 0x3F) << 6) | (c4 & 0x3F)));
         }
         else
         {
-          throw soci_error("Invalid UTF-8 encoding");
+          throw soci_error("Invalid UTF-8 sequence");
         }
-
-        utf32.push_back(cp);
       }
 
       return utf32;
@@ -241,7 +278,7 @@ namespace soci
 
     /**
      * @brief Converts a UTF-32 encoded string to a UTF-8 encoded string.
-     * 
+     *
      * @param utf32 The UTF-32 encoded string.
      * @return std::string The UTF-8 encoded string.
      * @throws soci_error if the input string contains invalid UTF-32 code points.
@@ -249,29 +286,31 @@ namespace soci
     inline std::string utf32_to_utf8(const std::u32string &utf32)
     {
       std::string utf8;
-      for (uint32_t cp : utf32)
+      utf8.reserve(utf32.size() * 4);
+
+      for (char32_t codepoint : utf32)
       {
-        if (cp < 0x80)
-        { // 1-byte sequence
-          utf8.push_back(static_cast<char>(cp));
+        if (codepoint < 0x80)
+        {
+          utf8.push_back(static_cast<char>(codepoint));
         }
-        else if (cp < 0x800)
-        { // 2-byte sequence
-          utf8.push_back(0xC0 | ((cp >> 6) & 0x1F));
-          utf8.push_back(0x80 | (cp & 0x3F));
+        else if (codepoint < 0x800)
+        {
+          utf8.push_back(static_cast<char>(0xC0 | ((codepoint >> 6) & 0x1F)));
+          utf8.push_back(static_cast<char>(0x80 | (codepoint & 0x3F)));
         }
-        else if (cp < 0x10000)
-        { // 3-byte sequence
-          utf8.push_back(0xE0 | ((cp >> 12) & 0x0F));
-          utf8.push_back(0x80 | ((cp >> 6) & 0x3F));
-          utf8.push_back(0x80 | (cp & 0x3F));
+        else if (codepoint < 0x10000)
+        {
+          utf8.push_back(static_cast<char>(0xE0 | ((codepoint >> 12) & 0x0F)));
+          utf8.push_back(static_cast<char>(0x80 | ((codepoint >> 6) & 0x3F)));
+          utf8.push_back(static_cast<char>(0x80 | (codepoint & 0x3F)));
         }
-        else if (cp < 0x110000)
-        { // 4-byte sequence
-          utf8.push_back(0xF0 | ((cp >> 18) & 0x07));
-          utf8.push_back(0x80 | ((cp >> 12) & 0x3F));
-          utf8.push_back(0x80 | ((cp >> 6) & 0x3F));
-          utf8.push_back(0x80 | (cp & 0x3F));
+        else if (codepoint <= 0x10FFFF)
+        {
+          utf8.push_back(static_cast<char>(0xF0 | ((codepoint >> 18) & 0x07)));
+          utf8.push_back(static_cast<char>(0x80 | ((codepoint >> 12) & 0x3F)));
+          utf8.push_back(static_cast<char>(0x80 | ((codepoint >> 6) & 0x3F)));
+          utf8.push_back(static_cast<char>(0x80 | (codepoint & 0x3F)));
         }
         else
         {
@@ -281,7 +320,7 @@ namespace soci
 
       return utf8;
     }
-
+    
     /**
      * @brief Converts a UTF-8 encoded string to a wide string (wstring).
      * 
