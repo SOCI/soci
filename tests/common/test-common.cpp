40cb91756fe2ca139e73d70fc3821f560ae28283
Vadim Zeitlin <vz-soci@zeitlins.org>
2024-11-06 15:46:04 +0100

    Extract CHECK_EQUAL_PADDED() in its own header too
    This prepares for its reuse elsewhere.
    
    Note that ideally the checks using this macro should be rewritten to use
    CATCH matchers instead, which would result in much better output in case
    of a failing test, but for now don't change anything and just move the
    code around.

--- tests/common/test-common.cpp
+++ tests/common/test-common.cpp
@@ -36,50 +36,7 @@
 
 #include "test-assert.h"
 #include "test-myint.h"
-
-// Although SQL standard mandates right padding CHAR(N) values to their length
-// with spaces, some backends don't confirm to it:
-//
-//  - Firebird does pad the string but to the byte-size (not character size) of
-//  the column (i.e. CHAR(10) NONE is padded to 10 bytes but CHAR(10) UTF8 --
-//  to 40).
-//  - For MySql PAD_CHAR_TO_FULL_LENGTH option must be set, otherwise the value
-//  is trimmed.
-//  - SQLite never behaves correctly at all.
-//
-// This method will check result string from column defined as fixed char It
-// will check only bytes up to the original string size. If padded string is
-// bigger than expected string then all remaining chars must be spaces so if
-// any non-space character is found it will fail.
-void
-checkEqualPadded(const std::string& padded_str, const std::string& expected_str)
-{
-    size_t const len = expected_str.length();
-    std::string const start_str(padded_str, 0, len);
-
-    if (start_str != expected_str)
-    {
-        throw soci::soci_error(
-                "Expected string \"" + expected_str + "\" "
-                "is different from the padded string \"" + padded_str + "\""
-              );
-    }
-
-    if (padded_str.length() > len)
-    {
-        std::string const end_str(padded_str, len);
-        if (end_str != std::string(padded_str.length() - len, ' '))
-        {
-            throw soci::soci_error(
-                  "\"" + padded_str + "\" starts with \"" + padded_str +
-                  "\" but non-space characater(s) are found aftewards"
-                );
-        }
-    }
-}
-
-#define CHECK_EQUAL_PADDED(padded_str, expected_str) \
-    CHECK_NOTHROW(checkEqualPadded(padded_str, expected_str));
+#include "test-padded.h"
 
 // Objects used later in tests 14,15
 struct PhonebookEntry
---
+++ tests/test-padded.h
@@ -0,0 +1,55 @@
+//
+// Copyright (C) 2004-2008 Maciej Sobczak, Stephen Hutton, Vadim Zeitlin
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef SOCI_TEST_PADDED_H_INCLUDED
+#define SOCI_TEST_PADDED_H_INCLUDED
+
+// Although SQL standard mandates right padding CHAR(N) values to their length
+// with spaces, some backends don't confirm to it:
+//
+//  - Firebird does pad the string but to the byte-size (not character size) of
+//  the column (i.e. CHAR(10) NONE is padded to 10 bytes but CHAR(10) UTF8 --
+//  to 40).
+//  - For MySql PAD_CHAR_TO_FULL_LENGTH option must be set, otherwise the value
+//  is trimmed.
+//  - SQLite never behaves correctly at all.
+//
+// This method will check result string from column defined as fixed char It
+// will check only bytes up to the original string size. If padded string is
+// bigger than expected string then all remaining chars must be spaces so if
+// any non-space character is found it will fail.
+inline void
+checkEqualPadded(const std::string& padded_str, const std::string& expected_str)
+{
+    size_t const len = expected_str.length();
+    std::string const start_str(padded_str, 0, len);
+
+    if (start_str != expected_str)
+    {
+        throw soci::soci_error(
+                "Expected string \"" + expected_str + "\" "
+                "is different from the padded string \"" + padded_str + "\""
+              );
+    }
+
+    if (padded_str.length() > len)
+    {
+        std::string const end_str(padded_str, len);
+        if (end_str != std::string(padded_str.length() - len, ' '))
+        {
+            throw soci::soci_error(
+                  "\"" + padded_str + "\" starts with \"" + padded_str +
+                  "\" but non-space characater(s) are found aftewards"
+                );
+        }
+    }
+}
+
+#define CHECK_EQUAL_PADDED(padded_str, expected_str) \
+    CHECK_NOTHROW(checkEqualPadded(padded_str, expected_str));
+
+#endif // SOCI_TEST_PADDED_H_INCLUDED
