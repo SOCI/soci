8b2cc353bc380543984b1111fdeecdbbab2703a6
Robert Adam <dev@robert-adam.de>
2025-02-27 18:59:45 +0100

    CI: test MariaDB

--- scripts/ci/before_build_mysql.sh
+++ scripts/ci/before_build_mysql.sh
@@ -8,17 +8,22 @@ source ${SOCI_SOURCE_DIR}/scripts/ci/common.sh
 SOCI_MYSQL_USER=$(id -un)
 
 if [ -n "${SOCI_MYSQL_ROOT_PASSWORD}" ]; then
-    sudo systemctl start mysql.service
+    if [[ "$SOCI_MYSQL_USE_MARIADB" = "YES" ]]; then
+        sudo systemctl start mariadb.service
+    else
+        sudo systemctl start mysql.service
+    fi
+
     SOCI_MYSQL_PASSWORD_OPT="-p${SOCI_MYSQL_ROOT_PASSWORD}"
-    mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "CREATE USER '${SOCI_MYSQL_USER}';"
+    sudo mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "CREATE USER '${SOCI_MYSQL_USER}';"
 fi
 
 mysql --version
-mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "CREATE DATABASE soci_test;"
-mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "GRANT ALL PRIVILEGES ON soci_test.* TO '${SOCI_MYSQL_USER}'@'%';";
+sudo mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "CREATE DATABASE soci_test;"
+sudo mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "GRANT ALL PRIVILEGES ON soci_test.* TO '${SOCI_MYSQL_USER}'@'%';";
 
 # This is necessary for MySQL stored procedure unit test to work.
-mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "SET GLOBAL log_bin_trust_function_creators=1";
+sudo mysql -u root ${SOCI_MYSQL_PASSWORD_OPT} -e "SET GLOBAL log_bin_trust_function_creators=1";
 
 echo 'Testing connection to the database:'
 echo 'SELECT USER();' | mysql --database=soci_test
