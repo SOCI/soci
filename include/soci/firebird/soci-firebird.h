ac622e4184ce70397e6382bd9b97505e9c0f98de
Vadim Zeitlin <vz-soci@zeitlins.org>
2025-04-24 20:22:27 +0200

    Implement get_error_category() for Firebird errors
    Recognize permission and constraint violation errors as well as a few
    other ones that haven't been actually observed but seem to match well
    the existing error categories.

--- include/soci/firebird/soci-firebird.h
+++ include/soci/firebird/soci-firebird.h
@@ -36,6 +36,8 @@ public:
     firebird_soci_error(std::string const & msg,
         ISC_STATUS const * status = 0);
 
+    error_category get_error_category() const override;
+
     std::string get_backend_name() const override { return "firebird"; }
     int get_backend_error_code() const override;
 
--- src/backends/firebird/error-firebird.cpp
+++ src/backends/firebird/error-firebird.cpp
@@ -61,6 +61,47 @@ int firebird_soci_error::get_backend_error_code() const
     return 0;
 }
 
+soci_error::error_category firebird_soci_error::get_error_category() const
+{
+    switch (get_backend_error_code())
+    {
+        case isc_bad_db_format:
+        case isc_unavailable:
+        case isc_wrong_ods:
+        case isc_badodsver:
+        case isc_connect_reject:
+        case isc_login:
+        case isc_network_error:
+        case isc_net_connect_err:
+        case isc_lost_db_connection:
+            return connection_error;
+
+        case isc_syntaxerr:
+        case isc_dsql_error:
+        case isc_command_end_err:
+            return invalid_statement;
+
+        case isc_no_priv:
+            return no_privilege;
+
+        case isc_not_valid:
+        case isc_no_dup:
+        case isc_foreign_key:
+        case isc_primary_key_ref:
+        case isc_primary_key_notnull:
+            return constraint_violation;
+
+        case isc_tra_state:
+            return unknown_transaction_state;
+
+        case isc_io_error:
+        case isc_virmemexh:
+            return system_error;
+    }
+
+    return unknown;
+}
+
 namespace details
 {
 
