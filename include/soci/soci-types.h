f2cad0bd74f165eb65a9940a0c5fcef883588aa2
Vadim Zeitlin <vz-soci@zeitlins.org>
2025-06-07 20:04:45 +0200

    Simplify checks for "long" type size
    We don't need to determine sizeof(long) at configure time when we can do
    it directly during the build using std::conditional<>.
    
    Also merge the identical specializations of various types for long in 32
    and 64-bit cases.

--- include/soci/soci-types.h
+++ include/soci/soci-types.h
@@ -1,31 +1,12 @@
 #ifndef SOCI_TYPES_H_INCLUDED
 #define SOCI_TYPES_H_INCLUDED
 
-#include "soci/soci-platform.h"
+#include <cstdint>
+#include <type_traits>
 
-// This should normally be defined in soci-config.h included from
-// soci-platform.h, but provide defaults for legacy non-CMake builds.
-#ifndef SOCI_SIZEOF_LONG
-    #if defined(_WIN32) || defined(_WIN64)
-        // Under Windows long is always 32 bits, even for 64-bit builds.
-        #define SOCI_SIZEOF_LONG 4
-    #elif defined(__LP64__)
-        // Note that currently we assume that all non-macOS Unix systems with
-        // 64-bit long int64_t define it as long, which seems to be true for
-        // all supported platforms.
-        #ifndef __APPLE__
-            #define SOCI_INT64_T_IS_LONG
-        #endif
-        #define SOCI_SIZEOF_LONG 8
-    #else
-        // Also note that on 32-bit platforms long is never the same thing as
-        // int32_t, as the latter is defined as int instead.
-        #define SOCI_SIZEOF_LONG 4
-    #endif
-#endif
-
-#if SOCI_SIZEOF_LONG == 8
-    #define SOCI_LONG_IS_64_BIT 1
-#endif
+// Define typedefs for std::[u]intN_t type of the same size as [unsigned] long
+// assuming it can only be 32 or 64 bits.
+using soci_long_t = std::conditional<sizeof(long) == 8, int64_t, int32_t>::type;
+using soci_ulong_t = std::conditional<sizeof(long) == 8, uint64_t, uint32_t>::type;
 
 #endif // SOCI_TYPES_H_INCLUDED
