SOCI/soci

commit 42c80b61ca251f1bada4f4acdd055132b760cd69

Fix bug in cstring_to_integer() for out of range inputs
Check for ERANGE which is the only way to detect a value out of range of
long long type.

include/private/soci-cstrtoi.h
@@ -10,6 +10,7 @@
 
 #include "soci/error.h"
 
+#include <cerrno>
 #include <cstdlib>
 #include <limits>
 
@@ -30,7 +31,7 @@ bool cstring_to_integer(T& result, char const* buf)
     char * end;
     const long long t = strtoll(buf, &end, 10);
 
-    if (end == buf || *end != '\0')
+    if (end == buf || *end != '\0' || errno == ERANGE)
         return false;
 
     // successfully converted to long long
@@ -53,7 +54,7 @@ bool cstring_to_unsigned(T& result, char const* buf)
     char * end;
     const unsigned long long t = strtoull(buf, &end, 10);
 
-    if (end == buf || *end != '\0')
+    if (end == buf || *end != '\0' || errno == ERANGE)
         return false;
 
     // successfully converted to unsigned long long

commit e2c19c2a17cd674bce0699f8a5cf1e71e70ab1ee

Fix error checking in cstring_to_integer() and cstring_to_unsigned()
Set errno to 0 to avoid spurious errors if strto[u]ll() succeeded.

This corrects a regression introduced in 42c80b61 (Fix bug in
cstring_to_integer() for out of range inputs, 2026-04-28).

include/private/soci-cstrtoi.h
@@ -29,6 +29,7 @@ template <typename T>
 bool cstring_to_integer(T& result, char const* buf)
 {
     char * end;
+    errno = 0;
     const long long t = strtoll(buf, &end, 10);
 
     if (end == buf || *end != '\0' || errno == ERANGE)
@@ -52,6 +53,7 @@ template <typename T>
 bool cstring_to_unsigned(T& result, char const* buf)
 {
     char * end;
+    errno = 0;
     const unsigned long long t = strtoull(buf, &end, 10);
 
     if (end == buf || *end != '\0' || errno == ERANGE)