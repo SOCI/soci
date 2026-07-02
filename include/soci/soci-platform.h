SOCI/soci

commit c35b1b6a77bafd2f9411aef7a082c88509ea6909

Avoid disabling MSVC CRT security warnings globally
Use "_s" versions of the standard C functions if possible/simple to do
and disable deprecation warnings explicitly in a couple of places where
this could not be done instead of disabling them globally.

Also stop using strcpy() entirely and use strncpy() instead.

See #1268.

include/soci/soci-platform.h
@@ -8,11 +8,6 @@
 #ifndef SOCI_PLATFORM_H_INCLUDED
 #define SOCI_PLATFORM_H_INCLUDED
 
-//disable MSVC deprecated warnings
-#if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
 #include <stdarg.h>
 #include <string.h>
 #include <string>
@@ -120,5 +115,48 @@ private: \
 #else
     #define SOCI_DUMMY_RETURN(x) return x
 #endif
+
+// Provide some wrappers for standard functions avoiding deprecation warnings
+// from MSVC.
+namespace soci
+{
+
+#ifdef _MSC_VER
+
+inline const char* getenv(const char* name)
+{
+  #pragma warning(push)
+  #pragma warning(disable:4996)
+
+  return std::getenv(name);
+
+  #pragma warning(pop)
+}
+
+inline int sscanf(const char* str, const char* format, ...)
+{
+  va_list args;
+  va_start(args, format);
+
+  const int result = vsscanf_s(str, format, args);
+
+  va_end(args);
+  return result;
+}
+
+inline char* strncpy(char* dest, const char* src, size_t n)
+{
+  strncpy_s(dest, n, src, _TRUNCATE);
+  return dest;
+}
+
+#else // !_MSC_VER
+
+using std::getenv;
+using std::sscanf;
+using std::strncpy;
+
+#endif // MSC_VER/!_MSC_VER
+}
 
 #endif // SOCI_PLATFORM_H_INCLUDED

cmake/soci_compiler_options.cmake
@@ -14,14 +14,6 @@ set(SOCI_LD "" CACHE STRING "Specify a non-default linker")
 
 
 if (WIN32)
-  target_compile_definitions(soci_compiler_interface
-    INTERFACE
-      _CRT_SECURE_NO_DEPRECATE
-      _CRT_SECURE_NO_WARNINGS
-      _CRT_NONSTDC_NO_WARNING
-      _SCL_SECURE_NO_WARNINGS
-  )
-
   # Prevent the Windows header files from defining annoying macros
   # and also cut down on the definitions in general
   target_compile_definitions(soci_compiler_interface

include/private/soci-cstrtod.h
@@ -51,7 +51,7 @@ double cstring_to_double(char const* s)
         // don't seem to be used in practice, so for now keep things simple.
         size_t const bufSize = strlen(s) + 1;
         char* const buf = new char[bufSize];
-        strcpy(buf, s);
+        strncpy(buf, s, bufSize);
         buf[end - s] = ',';
         d = strtod(buf, &end);
         parsedOK = end != buf && *end == '\0';

include/soci/odbc/soci-odbc.h
@@ -23,7 +23,6 @@
 #include <windows.h>
 #endif
 #include <sqlext.h> // ODBC
-#include <string.h> // strcpy()
 
 namespace soci
 {

src/backends/db2/session.cpp
@@ -123,7 +123,7 @@ db2_session_backend::db2_session_backend(
       // one of SQL_DRIVER_XXX constants but don't check for the exact value in
       // case more of them are added in the future, the ODBC driver will return
       // an error if we pass it an invalid value anyhow.
-      if (std::sscanf(completionString.c_str(), "%u", &completion) != 1)
+      if (soci::sscanf(completionString.c_str(), "%u", &completion) != 1)
       {
         throw soci_error("Invalid non-numeric driver completion option value \"" +
                           completionString + "\".");

src/backends/firebird/common.cpp
@@ -138,14 +138,14 @@ void setTextParam(char const * s, std::size_t size, char * buf_,
             || sqltype == SQL_TYPE_DATE)
     {
         unsigned short year, month, day, hour, min, sec;
-        if (std::sscanf(s, "%hu-%hu-%hu %hu:%hu:%hu",
+        if (soci::sscanf(s, "%hu-%hu-%hu %hu:%hu:%hu",
                     &year, &month, &day, &hour, &min, &sec) != 6)
         {
-            if (std::sscanf(s, "%hu-%hu-%huT%hu:%hu:%hu",
+            if (soci::sscanf(s, "%hu-%hu-%huT%hu:%hu:%hu",
                         &year, &month, &day, &hour, &min, &sec) != 6)
             {
                 hour = min = sec = 0;
-                if (std::sscanf(s, "%hu-%hu-%hu", &year, &month, &day) != 3)
+                if (soci::sscanf(s, "%hu-%hu-%hu", &year, &month, &day) != 3)
                 {
                     throw soci_error("Could not parse timestamp value.");
                 }
@@ -165,7 +165,7 @@ void setTextParam(char const * s, std::size_t size, char * buf_,
     else if (sqltype == SQL_TYPE_TIME)
     {
         unsigned short hour, min, sec;
-        if (std::sscanf(s, "%hu:%hu:%hu", &hour, &min, &sec) != 3)
+        if (soci::sscanf(s, "%hu:%hu:%hu", &hour, &min, &sec) != 3)
         {
             throw soci_error("Could not parse timestamp value.");
         }

src/backends/mysql/standard-use-type.cpp
@@ -46,7 +46,7 @@ void mysql_standard_use_type_backend::pre_use(indicator const *ind)
     if (ind != NULL && *ind == i_null)
     {
         buf_ = new char[5];
-        std::strcpy(buf_, "NULL");
+        strncpy(buf_, "NULL", 5);
     }
     else
     {
@@ -145,7 +145,7 @@ void mysql_standard_use_type_backend::pre_use(indicator const *ind)
                 std::string const s = double_to_cstring(d);
 
                 buf_ = new char[s.size() + 1];
-                std::strcpy(buf_, s.c_str());
+                strncpy(buf_, s.c_str(), s.size() + 1);
             }
             break;
         case x_stdtm:

src/backends/mysql/vector-use-type.cpp
@@ -55,7 +55,7 @@ void mysql_vector_use_type_backend::pre_use(indicator const *ind)
         if (ind != NULL && ind[i] == i_null)
         {
             buf = new char[5];
-            std::strcpy(buf, "NULL");
+            strncpy(buf, "NULL", 5);
         }
         else
         {
@@ -195,7 +195,7 @@ void mysql_vector_use_type_backend::pre_use(indicator const *ind)
                     std::string const s = double_to_cstring(v[i]);
 
                     buf = new char[s.size() + 1];
-                    std::strcpy(buf, s.c_str());
+                    strncpy(buf, s.c_str(), s.size() + 1);
                 }
                 break;
             case x_stdtm:

commit 426a26c31f1964b26d29ca9c7231d66658214792

Fix using bulk operations with strings in Oracle backend
This was broken by c35b1b6a (Avoid disabling MSVC CRT security warnings
globally, 2025-06-08) which implicitly replaced std::strncpy() calls in
Oracle vector use type backend with the calls to soci::strncpy() which
doesn't behave in the same way as the standard function, e.g. it can't
be used with n==0 and it always NUL-terminates the destination buffer.

Restore the original strncpy() behaviour to fix this.

See #1268.

include/soci/soci-platform.h
@@ -160,8 +160,12 @@ inline int sscanf(const char* str, const char* format, ...)
 
 inline char* strncpy(char* dest, const char* src, size_t n)
 {
-  strncpy_s(dest, n, src, _TRUNCATE);
-  return dest;
+  #pragma warning(push)
+  #pragma warning(disable:4996)
+
+  return std::strncpy(dest, src, n);
+
+  #pragma warning(pop)
 }
 
 #else // !_MSC_VER